5e4ea1b4c67ee078cba57e31ba478474da2aa1a8
Anubhav Jain <ajain@lbl.gov>
2013-03-20 14:53:06 -0700

    restore gzipped=False to custodian task

--- mpworks/firetasks/custodian_task.py
+++ mpworks/firetasks/custodian_task.py
@@ -32,7 +32,7 @@ class CustodianTask(FireTaskBase, FWSerializable):
         if 'static_run' in fw_spec['task_type']:
             jobs = VaspJob(v_exe,suffix=".static")  # TODO: fix this
         elif 'optimize structure (2x)' in fw_spec['task_type']:
-            jobs = VaspJob.double_relaxation_run(v_exe)
+            jobs = VaspJob.double_relaxation_run(v_exe, gzipped=False)
 
         c = Custodian(handlers, jobs, max_errors=10)
         error_details = c.run()
