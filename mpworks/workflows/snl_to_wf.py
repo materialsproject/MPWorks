4539de623a66e2549d4c548f8cff58d4093c2b43
Anubhav Jain <ajain@lbl.gov>
2013-03-21 22:06:29 -0700

    try adding a basic GGA+U-only workflow (no auto DB insertion)

--- mpworks/workflows/snl_to_wf.py
+++ mpworks/workflows/snl_to_wf.py
@@ -17,10 +17,10 @@ __date__ = 'Mar 15, 2013'
 import numpy as np
 
 
-def _snl_to_spec(snl, testing=False):
+def _snl_to_spec(snl, enforce_gga=True, testing=False):
     spec = {}
 
-    mpvis = MaterialsProjectGGAVaspInputSet()
+    mpvis = MaterialsProjectGGAVaspInputSet() if enforce_gga else MaterialsProjectVaspInputSet()
     structure = snl.structure
 
     spec['vasp'] = {}
@@ -31,7 +31,8 @@ def _snl_to_spec(snl, testing=False):
 
     spec['vaspinputset_name'] = mpvis.__class__.__name__
     # TODO: put entire vaspinputset.to_dict in the WF metadata?
-    spec['task_type'] = 'GGA optimize structure (2x)'
+
+    spec['task_type'] = 'GGA+U optimize structure (2x)' if spec['vasp']['incar'].get('LDAU', False) else 'GGA optimize structure (2x)'
     spec['snl'] = snl.to_dict
     spec['snl_id'] = -1
     spec['snl_group'] = -2
@@ -45,19 +46,16 @@ def _snl_to_spec(snl, testing=False):
     return spec
 
 
-def _snl_to_fw(snl, fw_id=-1, testing=False):
-    spec = _snl_to_spec(snl, testing)
-    tasks = [VASPWriterTask(), CustodianTask()]
-    return FireWork(tasks, spec, fw_id=fw_id)
-
-
 def snl_to_wf(snl, testing=False):
     # TODO: clean this up once we're out of testing mode
     # TODO: add WF metadata
     fws = []
     connections = {}
 
-    fws.append(_snl_to_fw(snl, fw_id=-1, testing=testing))  # add the GGA FireWork to the workflow
+    # add the root FW (GGA)
+    spec = _snl_to_spec(snl, enforce_gga=True, testing=testing)
+    tasks = [VASPWriterTask(), CustodianTask()]
+    fws.append(FireWork(tasks, spec, fw_id=-1))
 
     # determine if GGA+U FW is needed
     mpvis = MaterialsProjectVaspInputSet()
@@ -68,14 +66,16 @@ def snl_to_wf(snl, testing=False):
         fws.append(FireWork([VASPCopyTask({'extension': '.relax2'}), SetupGGAUTask(), CustodianTask()], spec, fw_id=-2))
         connections[-1] = -2
         spec = {'task_type': 'GGA+U static'}  # TODO: add more spec keys? SNL, etc?
-        fws.append(FireWork([VASPCopyTask({'extension': '.relax2'}), SetupStaticRunTask(), CustodianTask()], spec, fw_id=-3))
+        fws.append(
+            FireWork([VASPCopyTask({'extension': '.relax2'}), SetupStaticRunTask(), CustodianTask()], spec, fw_id=-3))
         connections[-2] = -3
         spec = {'task_type': 'GGA+U DOS'}  # TODO: add more spec keys? SNL, etc?
         fws.append(FireWork([VASPCopyTask(), SetupDOSRunTask(), CustodianTask()], spec, fw_id=-4))
         connections[-3] = -4
     else:
         spec = {'task_type': 'GGA static'}  # TODO: add more spec keys? SNL, etc?
-        fws.append(FireWork([VASPCopyTask({'extension': '.relax2'}), SetupStaticRunTask(), CustodianTask()], spec, fw_id=-3))
+        fws.append(
+            FireWork([VASPCopyTask({'extension': '.relax2'}), SetupStaticRunTask(), CustodianTask()], spec, fw_id=-3))
         connections[-1] = -3
         spec = {'task_type': 'GGA DOS'}  # TODO: add more spec keys? SNL, etc?
         fws.append(FireWork([VASPCopyTask(), SetupDOSRunTask(), CustodianTask()], spec, fw_id=-4))
@@ -83,8 +83,24 @@ def snl_to_wf(snl, testing=False):
 
     # TODO: check if gap > 1 eV before adding static run
 
-
     return Workflow(fws, connections)
+
+
+def snl_to_ggau_wf(snl):
+    """
+    This is a special workflow intended specifically for testing whether to run GGA+U runs without first running GGA>
+
+    TODO: delete this (or at least comment it out) once we're done with tests.
+    :param snl:
+    :param testing:
+    :return:
+    """
+
+    # add the root FW (GGA)
+    spec = _snl_to_spec(snl, enforce_gga=False, testing=False)
+    tasks = [VASPWriterTask(), CustodianTask()]
+    fw = FireWork(tasks, spec)
+    return Workflow.from_FireWork(fw)
 
 
 if __name__ == '__main__':
@@ -96,3 +112,5 @@ if __name__ == '__main__':
 
     snl_to_wf(snl1, testing=True).to_file('test_wfs/wf_si.json', indent=4)
     snl_to_wf(snl2, testing=True).to_file('test_wfs/wf_feo.json', indent=4)
+
+    snl_to_ggau_wf(snl2).to_file('test_wfs/wf_feo_GGAU.json', indent=4)
