e22512fac262bf9defee507b75b90889ae947984
Anubhav Jain <ajain@lbl.gov>
2013-03-22 14:41:00 -0700

    put dupefinder key in correct loc

--- mpworks/workflows/snl_to_wf_prod.py
+++ mpworks/workflows/snl_to_wf_prod.py
@@ -17,7 +17,7 @@ __email__ = 'ajain@lbl.gov'
 __date__ = 'Mar 15, 2013'
 
 
-def _snl_to_spec(snl, enforce_gga=True, inaccurate=False, dupecheck=True):
+def _snl_to_spec(snl, enforce_gga=True, inaccurate=False):
     spec = {}
 
     mpvis = MaterialsProjectGGAVaspInputSet() if enforce_gga else MaterialsProjectVaspInputSet()
@@ -28,12 +28,12 @@ def _snl_to_spec(snl, enforce_gga=True, inaccurate=False, dupecheck=True):
     spec['vasp']['poscar'] = mpvis.get_poscar(structure).to_dict
     spec['vasp']['kpoints'] = mpvis.get_kpoints(structure).to_dict
     spec['vasp']['potcar'] = mpvis.get_potcar(structure).to_dict
-
+    spec['_dupefinder'] = DupeFinderVASP().to_dict()
     spec['vaspinputset_name'] = mpvis.__class__.__name__
 
     spec['task_type'] = 'GGA+U optimize structure (2x)' if spec['vasp']['incar'].get('LDAU', False) else 'GGA optimize structure (2x)'
 
-    spec.update(_get_metadata(snl, verbose=True, dupecheck=dupecheck))
+    spec.update(_get_metadata(snl, verbose=True))
 
     #  override parameters for testing
     if inaccurate:
@@ -42,34 +42,32 @@ def _snl_to_spec(snl, enforce_gga=True, inaccurate=False, dupecheck=True):
     return spec
 
 
-def _get_metadata(snl, verbose=False, dupecheck=True):
+def _get_metadata(snl, verbose=False):
     md = {'run_tags': ['auto generation v1.0']}
     if verbose:
         md['snl'] = snl.to_dict
 
-    if dupecheck:
-        try:
-            sd = snl.data['_materialsproject']
-            md['snl_id'] = sd['snl_id']
-            md['snlgroup_id'] = sd['snlgroup_id']
-            md['snlgroupSG_id'] = sd['snlgroupSG_id']
-            md['_dupefinder'] = DupeFinderVASP().to_dict()
-        except:
-            raise ValueError("SNL must contain snl_id, snlgroup_id, snlgroupSG_id in data['_materialsproject']['snl_id']")
+    try:
+        sd = snl.data['_materialsproject']
+        md['snl_id'] = sd['snl_id']
+        md['snlgroup_id'] = sd['snlgroup_id']
+        md['snlgroupSG_id'] = sd['snlgroupSG_id']
+    except:
+        raise ValueError("SNL must contain snl_id, snlgroup_id, snlgroupSG_id in data['_materialsproject']['snl_id']")
 
     return md
 
 
-def snl_to_wf(snl, inaccurate=False, dupecheck=True):
+def snl_to_wf(snl, inaccurate=False):
     # TODO: clean this up once we're out of testing mode
     # TODO: add WF metadata
     fws = []
     connections = {}
     # add the root FW (GGA)
-    spec = _snl_to_spec(snl, enforce_gga=True, inaccurate=inaccurate, dupecheck=dupecheck)
+    spec = _snl_to_spec(snl, enforce_gga=True, inaccurate=inaccurate)
     tasks = [VASPWriterTask(), CustodianTask()]
     fws.append(FireWork(tasks, spec, fw_id=-1))
-    wf_meta = _get_metadata(snl, dupecheck=dupecheck)
+    wf_meta = _get_metadata(snl)
 
     """
     # determine if GGA+U FW is needed
@@ -77,29 +75,29 @@ def snl_to_wf(snl, inaccurate=False, dupecheck=True):
     incar = mpvis.get_incar(snl.structure).to_dict
     if 'LDAU' in incar and incar['LDAU']:
         spec = {'task_type': 'GGA+U optimize structure (2x)'}
-        spec.update(_get_metadata(snl, dupecheck=dupecheck))
+        spec.update(_get_metadata(snl))
         fws.append(FireWork([VASPCopyTask({'extension': '.relax2'}), SetupGGAUTask(), CustodianTask()], spec, fw_id=-2))
         connections[-1] = -2
 
         spec = {'task_type': 'GGA+U static'}
-        spec.update(_get_metadata(snl, dupecheck=dupecheck))
+        spec.update(_get_metadata(snl))
         fws.append(
             FireWork([VASPCopyTask({'extension': '.relax2'}), SetupStaticRunTask(), CustodianTask()], spec, fw_id=-3))
         connections[-2] = -3
 
         spec = {'task_type': 'GGA+U DOS'}
-        spec.update(_get_metadata(snl, dupecheck=dupecheck))
+        spec.update(_get_metadata(snl))
         fws.append(FireWork([VASPCopyTask(), SetupDOSRunTask(), CustodianTask()], spec, fw_id=-4))
         connections[-3] = -4
     else:
         spec = {'task_type': 'GGA static'}
-        spec.update(_get_metadata(snl, dupecheck=dupecheck))
+        spec.update(_get_metadata(snl))
         fws.append(
             FireWork([VASPCopyTask({'extension': '.relax2'}), SetupStaticRunTask(), CustodianTask()], spec, fw_id=-3))
         connections[-1] = -3
 
         spec = {'task_type': 'GGA DOS'}
-        spec.update(_get_metadata(snl, dupecheck=dupecheck))
+        spec.update(_get_metadata(snl))
         fws.append(FireWork([VASPCopyTask(), SetupDOSRunTask(), CustodianTask()], spec, fw_id=-4))
         connections[-3] = -4
 
