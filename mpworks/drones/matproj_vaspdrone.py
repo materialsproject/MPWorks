90bf7ea91a99cf7c10d7db1775ed10dc6a6a8777
Anubhav Jain <ajain@lbl.gov>
2013-03-26 16:42:30 -0700

    sketch out MatprojDrone - same thing as VTDBDD but with override on post_process

---
+++ mpworks/drones/matproj_vaspdrone.py
@@ -0,0 +1,19 @@
+from matgendb.creator import VaspToDbTaskDrone
+
+__author__ = 'Anubhav Jain'
+__copyright__ = 'Copyright 2013, The Materials Project'
+__version__ = '0.1'
+__maintainer__ = 'Anubhav Jain'
+__email__ = 'ajain@lbl.gov'
+__date__ = 'Mar 26, 2013'
+
+
+class MatprojVaspDrone(VaspToDbTaskDrone):
+
+    @classmethod
+    def post_process(cls, dir_name, d):
+        # run the post-process of the superclass
+        VaspToDbTaskDrone.post_process(dir_name, d)
+
+        # custom Materials Project post-processing
+        d['branding'] = 'MATERIALS PROJECT IS DA BEST PROJECT'
