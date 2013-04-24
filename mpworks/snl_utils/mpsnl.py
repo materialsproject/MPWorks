43e3113e94da2c5136238c0196b4806e51ab67f1
Anubhav Jain <ajain@lbl.gov>
2013-04-24 11:55:17 -0700

    sketch out a few custom SNL classes for MP, e.g. with ids for storing in a db and also storing unique structures as SNLGroup

---
+++ mpworks/snl_utils/mpsnl.py
@@ -0,0 +1,145 @@
+import itertools
+import datetime
+from pymatgen import Structure, PMGJSONDecoder, Molecule, SiteCollection, Composition
+from pymatgen.analysis.structure_matcher import StructureMatcher, SpeciesComparator, ElementComparator
+from pymatgen.matproj.snl import StructureNL
+
+__author__ = 'Anubhav Jain'
+__copyright__ = 'Copyright 2013, The Materials Project'
+__version__ = '0.1'
+__maintainer__ = 'Anubhav Jain'
+__email__ = 'ajain@lbl.gov'
+__date__ = 'Apr 24, 2013'
+
+
+def get_meta_from_structure(structure):
+    # TODO: this won't work for molecules
+    meta = {}
+    meta['nsites'] = len(structure.sites)
+    meta['elements'] = list(set([el.symbol for el in structure.composition.elements]))
+    meta['nelements'] = len(meta['elements'])
+    meta['formula'] = structure.composition.formula
+    meta['formula_red'] = structure.composition.reduced_formula
+    meta['formula_abc_red'] = Composition.from_formula(structure.composition.reduced_formula).alphabetical_formula
+    meta['composition_dict'] = structure.composition.to_dict
+    meta['anonymized_formula'] = structure.composition.anonymized_formula
+    meta['chemsystem'] = '-'.join(sorted(list(set([e.symbol for e in structure.composition.elements]))))  # the complex logic set/list is to prevent duplicates if there are multiple oxidation states
+    meta['is_ordered'] = structure.is_ordered
+
+    #promixity warning:
+    meta['proximity_warning'] = False
+    for (s1, s2) in itertools.combinations(structure._sites, 2):
+        if s1.distance(s2) < SiteCollection.DISTANCE_TOLERANCE:
+            meta['proximity_warning'] = True
+
+    return meta
+
+
+class MPStructureNL(StructureNL):
+    # adds snl_id, spacegroup, and autometa properties to StructureNL.
+
+    def __init__(self, snl_id, sg_num, *args, **kwargs):
+        super(MPStructureNL, self).__init__(*args, **kwargs)
+        self.snl_id = snl_id
+        self.sg_num = sg_num
+        self.autometa = get_meta_from_structure(self.structure)
+
+    @property
+    def snlgroup_key(self):
+        return self.autometa['formula_abc_red'] + "--" + str(self.sg_num)
+
+    @property
+    def to_dict(self):
+        m_dict = super(MPStructureNL, self).to_dict
+        m_dict['snl_id'] = self.snl_id
+        m_dict['sg_num'] = self.sg_num
+        m_dict['autometa'] = self.autometa
+        m_dict['snlgroup_key'] = self.snlgroup_key
+        return m_dict
+
+    @staticmethod
+    def from_dict(d):
+        a = d["about"]
+        dec = PMGJSONDecoder()
+
+        created_at = dec.process_decoded(a["created_at"]) if "created_at" in a \
+            else None
+        data = {k: v for k, v in d["about"].items()
+                if k.startswith("_")}
+        data = dec.process_decoded(data)
+
+        structure = Structure.from_dict(d) if "lattice" in d \
+            else Molecule.from_dict(d)
+        return MPStructureNL(d['snl_id'], d['sg_num'], structure, a["authors"],
+                           projects=a.get("projects", None),
+                           references=a.get("references", ""),
+                           remarks=a.get("remarks", None), data=data,
+                           history=a.get("history", None),
+                           created_at=created_at)
+
+class SNLGroup():
+    def __init__(self, snlgroup_id, canonical_snl, all_snl_ids=None):
+        # Auto fields
+        self.created_at = datetime.datetime.utcnow()
+        self.updated_at = datetime.datetime.utcnow()
+
+        # User fields
+        self.snlgroup_id = snlgroup_id
+        self.canonical_snl = canonical_snl
+
+        self.all_snl_ids = all_snl_ids if all_snl_ids else []
+        if self.canonical_snl.snl_id not in self.all_snl_ids:
+            self.all_mps_ids.append(canonical_snl.snl_id)
+
+        # Convenience fields
+        self.canonical_structure = canonical_snl.structure
+        self.autometa = get_meta_from_structure(self.canonical_structure)
+
+    def switch_canonical_mps(self, new_canonical_mps):
+        raise NotImplementedError()
+
+    def to_dict(self):
+        d = {}
+        d['created_at'] = self.created_at
+        d['updated_at'] = self.updated_at
+        d['snlgroup_id'] = self.snlgroup_id
+        d['canonical_snl'] = self.canonical_snl.to_dict
+        d['all_snl_ids'] = self.all_snl_ids
+        d['num_snl'] = len(self.all_snl_ids)
+        d['autometa'] = self.autometa
+        d['snlgroup_key'] = self.snlgroup_key
+        return d
+
+    @staticmethod
+    def from_dict(d):
+        return SNLGroup(d['snlgroup_id'], MPStructureNL.from_dict(d['canonical_snl']), d['all_snl_ids'])
+
+    def add_if_belongs(self, cand_snl):
+
+        # no need to compare if different formulas or spacegroups
+        if not cand_snl.mpsgroup_key != self.canonical_snl.mpsgroup_key:
+            return False
+
+        # no need to compare if one is ordered, the other disordered
+        if not (cand_snl.structure.is_ordered == self.canonical_structure.is_ordered):
+            return False
+
+        # make sure the structure is not already in all_structures
+        if cand_snl.snl_id in self.all_snl_ids:
+            print 'WARNING: add_if_belongs() has detected that you are trying to add the same SNL id twice!'
+            return False
+
+        #try a structure fit to the canonical structure
+
+        # use default Structure Matcher params from April 24, 2013, as suggested by Shyue
+        # we are using the ElementComparator() because this is how we want to group results
+        sm = StructureMatcher(ltol=0.2, stol=0.3, angle_tol=5, primitive_cell=True, scale=True, attempt_supercell=True, comparator=ElementComparator())
+
+        if not sm.fit(cand_snl.structure, self.canonical_structure):
+            return False
+
+        # everything checks out, add to the group
+        self.all_snl_ids.append(cand_snl.snl_id)
+        self.updated_at = datetime.datetime.utcnow()
+
+        return True
