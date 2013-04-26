0f680232a99f774c6c542782ee52af3818dfa76a
Anubhav Jain <ajain@lbl.gov>
2013-04-25 17:14:12 -0700

    switch back to os.getcwd(), was thinking the wrong thing

--- mpworks/firetasks/vasp_setup_tasks.py
+++ mpworks/firetasks/vasp_setup_tasks.py
@@ -36,7 +36,7 @@ class SetupStaticRunTask(FireTaskBase, FWSerializable):
 
         user_incar_settings={"NPAR":2}
 
-        MaterialsProjectStaticVaspInputSet.from_previous_vasp_run(fw_spec["prev_vasp_dir"], user_incar_settings=user_incar_settings)
+        MaterialsProjectStaticVaspInputSet.from_previous_vasp_run(os.getcwd(), user_incar_settings=user_incar_settings)
         structure = MaterialsProjectStaticVaspInputSet.get_structure(Vasprun("vasprun.xml"), Outcar("OUTCAR"),
                                                                      initial_structure=False, refined_structure=True)
         # redo POTCAR - this is necessary whenever you change a Structure
@@ -65,7 +65,7 @@ class SetupNonSCFTask(FireTaskBase, FWSerializable):
         try:
             vasp_run = Vasprun("vasprun.xml", parse_dos=False,
                                parse_eigen=False)
-            outcar = Outcar(os.path.join(fw_spec["prev_vasp_dir"],"OUTCAR"))
+            outcar = Outcar(os.path.join(os.getcwd(),"OUTCAR"))
         except Exception as e:
             raise RuntimeError("Can't get valid results from relaxed run: " + str(e))
 
