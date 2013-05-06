4bf3b9919cb051070bc0d1b41defecc7ecab1adf
Anubhav Jain <ajain@lbl.gov>
2013-05-06 11:46:30 -0700

    add draft of auto-submit tests script

---
+++ mpworks/scripts/submit_tests_run.py
@@ -0,0 +1,34 @@
+from mpworks.submissions.submit_tests import clear_and_submit
+
+__author__ = 'Anubhav Jain'
+__copyright__ = 'Copyright 2013, The Materials Project'
+__version__ = '0.1'
+__maintainer__ = 'Anubhav Jain'
+__email__ = 'ajain@lbl.gov'
+__date__ = 'May 06, 2013'
+
+"""
+A runnable script for submitting test jobs
+"""
+
+from argparse import ArgumentParser
+
+__author__ = "Anubhav Jain"
+__copyright__ = "Copyright 2013, The Materials Project"
+__version__ = "0.1"
+__maintainer__ = "Anubhav Jain"
+__email__ = "ajain@lbl.gov"
+__date__ = "Jan 14, 2013"
+
+
+def go_testing():
+    m_description = 'This program is used to clear and submit jobs from the database'
+
+    parser = ArgumentParser(description=m_description)
+    parser.add_argument('--clear', help='clear old databases', default=False, type=bool)
+    args = parser.parse_args()
+    
+    clear_and_submit(args.clear)
+
+if __name__ == '__main__':
+    go_testing()
---
+++ mpworks/submissions/submit_tests.py
@@ -0,0 +1,72 @@
+import json
+import os
+from pymongo import MongoClient
+from fireworks.core.launchpad import LaunchPad
+from mpworks.snl_utils.snl_mongo import SNLMongoAdapter
+from mpworks.submissions.submissions_mongo import SubmissionMongoAdapter
+from pymatgen import MPRester
+from pymatgen.matproj.snl import StructureNL
+
+__author__ = 'Anubhav Jain'
+__copyright__ = 'Copyright 2013, The Materials Project'
+__version__ = '0.1'
+__maintainer__ = 'Anubhav Jain'
+__email__ = 'ajain@lbl.gov'
+__date__ = 'May 06, 2013'
+
+
+def clear_env():
+    sma = SubmissionMongoAdapter.auto_load()
+    if 'testing' not in str(sma.db):
+        raise ValueError('{} is not a testing database'.format(sma.db))
+
+    lp = LaunchPad.auto_load()
+    if 'testing' not in str(lp.db):
+        raise ValueError('{} is not a testing database'.format(sma.db))
+
+    snl = SNLMongoAdapter.auto_load()
+    if 'testing' not in str(snl.db):
+        raise ValueError('{} is not a testing database'.format(sma.db))
+
+    db_dir = os.environ['DB_LOC']
+    db_path = os.path.join(db_dir, 'tasks_db.json')
+    with open(db_path) as f:
+        db_creds = json.load(f)
+        if 'testing' not in db_creds['database']:
+            raise ValueError('{} is not a testing database'.format(db_creds['database']))
+
+
+    sma._reset()
+    lp.reset('', require_password=False)
+    snl._reset()
+
+    conn = MongoClient(db_creds['host'], db_creds['port'])
+    db = conn[db_creds['database']]
+    db.authenticate(db_creds['admin_user'], db_creds['admin_password'])
+    db.tasks.remove()
+    db.counter.remove()
+    db['dos_fs.chunks'].remove()
+    db['dos_fs.files'].remove()
+
+
+def submit_tests():
+    sma = SubmissionMongoAdapter.auto_load()
+    if 'testing' not in str(sma.db):
+        raise ValueError('{} is not a testing database'.format(sma.db))
+
+    compounds={"Si": 149, "Al": 134}
+
+    sids = compounds.values()
+    mpr = MPRester(api_key="flebb3pU1yfExlOc", host="www.materialsproject.org")
+
+    for sid in sids:
+        s = mpr.get_structure_by_material_id(sid)
+
+        snl = StructureNL(s, 'Anubhav Jain <anubhavster@gmail.com>')
+        sma.submit_snl(snl, 'anubhavster@gmail.com', parameters=None)
+
+
+def clear_and_submit(clear=False):
+    if clear:
+        clear_env()
+    submit_tests()
