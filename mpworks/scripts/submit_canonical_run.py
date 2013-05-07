9f60c39d396473f524dc2d91e6e2e6556ceec6f1
Anubhav Jain <ajain@lbl.gov>
2013-05-07 09:47:39 -0700

    ok change name again, looks like nose is pretty aggressive about testing *anything* that has the word test somewhere in the file

--- mpworks/scripts/submit_canonical_run.py
+++ mpworks/scripts/submit_canonical_run.py
@@ -1,4 +1,4 @@
-from mpworks.submissions.submit_testjobs import clear_and_submit
+from mpworks.submissions.submit_canonical import clear_and_submit
 
 __author__ = 'Anubhav Jain'
 __copyright__ = 'Copyright 2013, The Materials Project'
