bcfc1aa5edb7334adb3a13e3014b4395fe0ba6a4
Anubhav Jain <ajain@lbl.gov>
2013-05-13 11:35:08 -0700

    enter ICSD 2012 into db for realz

--- mpworks/legacy/actions/do_icsd_to_snl.py
+++ mpworks/legacy/actions/do_icsd_to_snl.py
@@ -1,7 +1,7 @@
 import json
 import os
 import traceback
-from pymongo import MongoClient
+from pymongo import MongoClient, ASCENDING
 import yaml
 from mpworks.legacy.icsd2012_to_snl import icsd_dict_to_snl
 from mpworks.snl_utils.snl_mongo import SNLMongoAdapter
@@ -28,14 +28,12 @@ if __name__ == '__main__':
 
     db.authenticate(y['username'], y['password'])
 
-    # snldb = SNLMongoAdapter.from_file(snl_f)
+    snldb = SNLMongoAdapter.from_file(snl_f)
 
-    for icsd_dict in db.icsd_2012_crystals.find(timeout=False):
+    for icsd_dict in db.icsd_2012_crystals.find(sort=[("icsd_id", ASCENDING)], timeout=False):
         try:
-                snl = icsd_dict_to_snl(icsd_dict)
-                print json.dumps(snl.to_dict, default=DATETIME_HANDLER)
-                #if snl:
-                #    snldb.add_snl(snl)
+            snl = icsd_dict_to_snl(icsd_dict)
+            snldb.add_snl(snl)
         except:
             traceback.print_exc()
             print 'ERROR - icsd id:', icsd_dict['icsd_id']
