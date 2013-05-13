1c7f06eb7af1df4ad95e5c18c7328140855b058a
Anubhav Jain <ajain@lbl.gov>
2013-05-12 20:52:17 -0700

    some SNL conversion updates

--- mpworks/legacy/actions/do_icsd_to_snl.py
+++ mpworks/legacy/actions/do_icsd_to_snl.py
@@ -1,6 +1,41 @@
+import os
+import traceback
+from pymongo import MongoClient
+import yaml
+from mpworks.legacy.icsd2012_to_snl import icsd_dict_to_snl
+from mpworks.snl_utils.snl_mongo import SNLMongoAdapter
+
 __author__ = 'Anubhav Jain'
 __copyright__ = 'Copyright 2013, The Materials Project'
 __version__ = '0.1'
 __maintainer__ = 'Anubhav Jain'
 __email__ = 'ajain@lbl.gov'
 __date__ = 'May 12, 2013'
+
+if __name__ == '__main__':
+
+    module_dir = os.path.dirname(os.path.abspath(__file__))
+    icsd_f = os.path.join(module_dir, 'icsd2012.yaml')
+    snl_f = os.path.join(module_dir, 'snl.yaml')
+
+    with open(icsd_f) as f:
+        y = yaml.load(f)
+
+    mc = MongoClient(y['host'], y['port'])
+    db = mc[y['db']]
+
+    db.authenticate(y['username'], y['password'])
+
+    # snldb = SNLMongoAdapter.from_file(snl_f)
+
+    for icsd_dict in db.icsd_2012_crystals.find(timeout=False):
+        try:
+                snl = icsd_dict_to_snl(icsd_dict)
+                print snl
+                #if snl:
+                #    snldb.add_snl(snl)
+        except:
+            traceback.print_exc()
+            print 'ERROR - icsd id:', icsd_dict['icsd_id']
+
+    print 'DONE'
--- mpworks/legacy/icsd2012_to_snl.py
+++ mpworks/legacy/icsd2012_to_snl.py
@@ -1,3 +1,9 @@
+import re
+import random
+import unicodedata
+from pymatgen import Structure
+from pymatgen.matproj.snl import StructureNL
+
 __author__ = 'Anubhav Jain'
 __copyright__ = 'Copyright 2013, The Materials Project'
 __version__ = '0.1'
@@ -7,4 +13,65 @@ __date__ = 'May 12, 2013'
 
 
 def icsd_dict_to_snl(icsd_dict):
-    return icsd_dict.keys()
+    struct = Structure.from_dict(icsd_dict['tstruct'])
+    references = _get_icsd_reference(icsd_dict)
+
+    projects = None
+    remarks = None
+    data = {}
+    history = None
+    authors = 'Will Richards <wrichard@mit.edu>, Anubhav Jain <ajain@lbl.gov>, Shyue Ping Ong <shyue@mit.edu>'
+
+    return StructureNL(struct, authors, projects, references, remarks, data, history)
+
+
+def _get_icsd_reference(icsd_dict):
+
+    if icsd_dict and 'journal' in icsd_dict and icsd_dict['journal']['authors']:
+        pages = ""
+        if icsd_dict['journal']['PAGE_FIRST']:
+            pages = str(icsd_dict['journal']['PAGE_FIRST'])
+
+        if icsd_dict['journal']['PAGE_LAST']:
+            pages = pages + "--" + str(icsd_dict['journal']['PAGE_LAST'])
+
+        bibtex_str = "@article{"
+        #author last name as key
+        m_key = icsd_dict['journal']['authors'][0]
+        m_key = re.sub(r'\s', '_', m_key)
+        m_key = m_key[0:m_key.find(',')]
+        bibtex_str += m_key
+        #year + random
+        bibtex_str += str(icsd_dict['journal']['YEAR']) + "_" + str(random.randrange(1, 1000)) + ",\n"
+
+        bibtex_str += "title = {{" + icsd_dict['au_title']+ "}},\n"
+
+        auth_str = "author = {" + " and ".join(icsd_dict['journal']['authors']) + "},\n"
+        # sanitize authors so there are no parentheses (weird ICSD conversion thing)
+        regex = re.compile('\(.+?\)')
+        auth_str = regex.sub('', auth_str)
+        bibtex_str += auth_str
+
+        if icsd_dict['journal']['YEAR']:
+            bibtex_str += "year = {" + str(icsd_dict['journal']['YEAR']) + "},\n"
+
+        if icsd_dict['journal']['J_TITLE']:
+            bibtex_str += "journal = {" + icsd_dict['journal']['J_TITLE'] + "},\n"
+
+        if icsd_dict['journal']['VOLUME']:
+            bibtex_str += "volume = {" + str(icsd_dict['journal']['VOLUME']) + "},\n"
+
+        if icsd_dict['journal']['ISSUE']:
+            bibtex_str += "issue = {" + str(icsd_dict['journal']['ISSUE']) + "},\n"
+        bibtex_str += "pages = {" + pages + "},\n"
+
+        if icsd_dict['journal']['ISSN']:
+            bibtex_str += "issn = " + icsd_dict['journal']['ISSN'] + "\n"
+
+        bibtex_str += "}"
+        bibtex_str = unicodedata.normalize('NFKD', bibtex_str).encode('ascii','ignore')
+
+        return bibtex_str
+
+
+    return None
