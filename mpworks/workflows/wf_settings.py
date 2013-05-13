aaad46c264cd6578337c7206cbc0d526708e6f95
Anubhav Jain <ajain@lbl.gov>
2013-05-12 18:43:29 -0700

    change queue adapter params

--- mpworks/workflows/wf_settings.py
+++ mpworks/workflows/wf_settings.py
@@ -5,6 +5,6 @@ __maintainer__ = 'Anubhav Jain'
 __email__ = 'ajain@lbl.gov'
 __date__ = 'May 10, 2013'
 
-QA_VASP = {'nnodes': 2, 'walltime': '96:00:00'}  # don't change nnodes unless other parts of code are also changed
-QA_DB = {'nnodes': 1, 'walltime': '24:00:00'}  # don't change nnodes unless other parts of code are also changed
-QA_CONTROL = {'nnodes': 2, 'walltime': '00:30:00'}  # don't change nnodes unless other parts of code are also changed
+QA_VASP = {'nnodes': 2}  # don't change nnodes unless other parts of code are also changed
+QA_DB = {'nnodes': 1, 'walltime': '24:00:00'}
+QA_CONTROL = {'nnodes': 1, 'walltime': '00:30:00'}
