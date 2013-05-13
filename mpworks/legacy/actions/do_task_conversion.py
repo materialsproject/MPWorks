87d19dc6439d9ff1f8de35b3f26b569f60a66085
Anubhav Jain <ajain@lbl.gov>
2013-05-13 15:58:43 -0700

    add a multiprocessing builder

--- mpworks/legacy/actions/do_task_conversion.py
+++ mpworks/legacy/actions/do_task_conversion.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import multiprocessing
 import os
 import traceback
 from pymongo import MongoClient, ASCENDING
@@ -13,37 +14,47 @@ __maintainer__ = 'Anubhav Jain'
 __email__ = 'ajain@lbl.gov'
 __date__ = 'May 13, 2013'
 
-module_dir = os.path.dirname(os.path.abspath(__file__))
-tasks_f = os.path.join(module_dir, 'mg_core_dev.yaml')
-tasks_new_f = os.path.join(module_dir, 'tasks_new.yaml')
 
-with open(tasks_f) as f:
-    y = yaml.load(f)
+def process_task(task_id):
+    module_dir = os.path.dirname(os.path.abspath(__file__))
+    tasks_f = os.path.join(module_dir, 'mg_core_dev.yaml')
+    tasks_new_f = os.path.join(module_dir, 'tasks_new.yaml')
 
-    mc = MongoClient(y['host'], y['port'])
-    db = mc[y['db']]
-    db.authenticate(y['username'], y['password'])
+    with open(tasks_f) as f:
+        y = yaml.load(f)
 
-    tasks_old = db['tasks_dbv2']
+        mc = MongoClient(y['host'], y['port'])
+        db = mc[y['db']]
+        db.authenticate(y['username'], y['password'])
 
-    # get the directory containing the db file
-    db_dir = os.environ['DB_LOC']
-    db_path = os.path.join(db_dir, 'tasks_db.json')
-    logging.basicConfig(level=logging.INFO)
-    with open(db_path) as f:
-        db_creds = json.load(f)
-        drone = MPVaspDrone_CONVERSION(
-        host=db_creds['host'], port=db_creds['port'],
-        database=db_creds['database'], user=db_creds['admin_user'],
-        password=db_creds['admin_password'],
-        collection=db_creds['collection'], parse_dos=False,
-        additional_fields={},
-        update_duplicates=False)
-        for t in tasks_old.find(sort=[("task_id", ASCENDING)], timeout=False):
-            # get the directory containing the db file
-            try:
-                t_id, d = drone.assimilate(t)
-                print 'ENTERED', t_id
-            except:
-                print 'ERROR entering', t['task_id']
-                traceback.print_exc()
+        tasks_old = db['tasks_dbv2']
+
+        # get the directory containing the db file
+        db_dir = os.environ['DB_LOC']
+        db_path = os.path.join(db_dir, 'tasks_db.json')
+        logging.basicConfig(level=logging.INFO)
+        with open(db_path) as f:
+            db_creds = json.load(f)
+            drone = MPVaspDrone_CONVERSION(
+            host=db_creds['host'], port=db_creds['port'],
+            database=db_creds['database'], user=db_creds['admin_user'],
+            password=db_creds['admin_password'],
+            collection=db_creds['collection'], parse_dos=False,
+            additional_fields={},
+            update_duplicates=False)
+            t = tasks_old.find_one({'task_id': task_id})
+            if t:
+                # get the directory containing the db file
+                try:
+                    t_id, d = drone.assimilate(t)
+                    print 'ENTERED', t_id
+                except:
+                    print 'ERROR entering', t['task_id']
+                    traceback.print_exc()
+
+
+def _parallel_build():
+    task_ids = range(1, 10000)
+    pool = multiprocessing.Pool(16)
+    pool.map(process_task, task_ids)
+    print 'DONE'
