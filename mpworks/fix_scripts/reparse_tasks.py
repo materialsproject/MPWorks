0c603319b0c7e8d2b973a0f28ab3d726e38389fb
Anubhav Jain <ajain@lbl.gov>
2013-06-13 19:56:54 -0700

    bugfix

--- mpworks/fix_scripts/reparse_tasks.py
+++ mpworks/fix_scripts/reparse_tasks.py
@@ -85,7 +85,7 @@ if __name__ == '__main__':
     tasks = TaskBuilder.tasks
     m_data = []
     q = {'submission_id': {'$exists': False}}  # these are all new-style tasks
-    for d in tasks.find(q, {'dir_name_full': 1, 'task_id': 1}):
+    for d in tasks.find(q, {'dir_name_full': 1, 'task_type': 1}):
         m_data.append((d['dir_name_full'], 'Uniform' in d['task_type']))
     print 'GOT all tasks...'
     pool = multiprocessing.Pool(16)
