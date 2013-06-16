71e9e2e7e74ce88ab5a74733982aca2d5bd67b21
Anubhav Jain <ajain@lbl.gov>
2013-06-16 14:49:26 -0700

    add custom pre-rocket for DB FWs

--- mpworks/workflows/wf_settings.py
+++ mpworks/workflows/wf_settings.py
@@ -6,7 +6,7 @@ __email__ = 'ajain@lbl.gov'
 __date__ = 'May 10, 2013'
 
 QA_VASP = {'nnodes': 2}  # don't change nnodes unless other parts of code are also changed
-QA_DB = {'nnodes': 1, 'walltime': '24:00:00'}
+QA_DB = {'nnodes': 1, 'walltime': '24:00:00',
+         'pre_rocket': '#PBS -V\nulimit -v hard\nmodule load python/2.7.3\nmodule swap numpy numpy/1.7.1\n'}
 QA_CONTROL = {'nnodes': 1, 'walltime': '00:30:00'}
-
 MOVE_TO_GARDEN = True
