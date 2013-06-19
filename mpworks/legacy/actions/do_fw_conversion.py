5600bcab5271f079f77d34b563b348730445973e
Anubhav Jain <ajain@lbl.gov>
2013-06-18 21:08:31 -0700

    comment out some lines, add index

--- mpworks/legacy/actions/do_fw_conversion.py
+++ mpworks/legacy/actions/do_fw_conversion.py
@@ -14,12 +14,12 @@ __date__ = 'May 14, 2013'
 if __name__ == '__main__':
 
     module_dir = os.path.dirname(os.path.abspath(__file__))
-    lp_f = os.path.join(module_dir, 'my_launchpad.yaml')
+    # lp_f = os.path.join(module_dir, 'my_launchpad.yaml')
     tasks_f = os.path.join(module_dir, 'tasks.yaml')
 
-    with open(lp_f) as f:
-        lp = LaunchPad.from_file(lp_f)
-        lp.reset(None, require_password=False)
+    #with open(lp_f) as f:
+    #    lp = LaunchPad.from_file(lp_f)
+    #    lp.reset(None, require_password=False)
 
     with open(tasks_f) as f2:
         db_creds = yaml.load(f2)
@@ -30,18 +30,21 @@ if __name__ == '__main__':
         new_tasks = db2['tasks']
 
     print new_tasks.count()
-    """
+
     new_tasks.ensure_index("task_id", unique=True)
     new_tasks.ensure_index("chemsys")
     new_tasks.ensure_index("analysis.e_above_hull")
     new_tasks.ensure_index("pretty_formula")
     new_tasks.ensure_index([("elements", ASCENDING), ("nelements", ASCENDING)])
     new_tasks.ensure_index("state")
+    new_tasks.ensure_index([("state", ASCENDING), ("task_type", ASCENDING)])
+    new_tasks.ensure_index([("state", ASCENDING), ("task_type", ASCENDING), ("submission_id", ASCENDING)])
     new_tasks.ensure_index("is_compatible")
     new_tasks.ensure_index("snl.snl_id")
     new_tasks.ensure_index("snlgroup_id")
-    """
 
+    """
     for task_dict in new_tasks.find({"state":"successful"}, sort=[("task_id", ASCENDING)], timeout=False):
         fw_id = task_dict_to_wf(task_dict, lp)
         new_tasks.update({"task_id": task_dict["task_id"]}, {"$set": {"fw_id": fw_id}})
+    """
