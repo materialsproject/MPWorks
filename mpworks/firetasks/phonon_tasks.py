4903042173eaeed54581c980d98e91ae0b5492cb
Wei Chen <weichen@lbl.gov>
2013-07-07 15:31:14 -0700

    add snlgroup_id for deformed structure

--- mpworks/firetasks/phonon_tasks.py
+++ mpworks/firetasks/phonon_tasks.py
@@ -15,6 +15,8 @@ from fireworks.utilities.fw_utilities import get_slug
 from pymatgen import Composition
 from pymatgen.matproj.snl import StructureNL
 from mpworks.workflows import snl_to_wf
+from mpworks.firetasks.snl_tasks import AddSNLTask
+from mpworks.snl_utils.mpsnl import get_meta_from_structure, MPStructureNL
 
 def update_spec_force_convergence(spec):
     fw_spec = spec
@@ -48,7 +50,7 @@ class SetupElastConstTask(FireTaskBase, FWSerializable):
         incar.write_file("INCAR")
         return FWAction()
 
-class SetupDeformedStructTask(FireTaskBase, FWSerializable):
+class GetDeformedStructTask(FireTaskBase, FWSerializable):
     _fw_name = "Setup Deformed Struct Task"
 
     def run_task(self, fw_spec):
@@ -60,22 +62,33 @@ class SetupDeformedStructTask(FireTaskBase, FWSerializable):
 
         for i, strain in enumerate(deformed_structs.keys()):
             d_struct = deformed_structs[strain]
-
             f = Composition.from_formula(d_struct.formula).alphabetical_formula
             snl = StructureNL(d_struct, 'Wei Chen <weichen@lbl.gov>')
+
+            tasks = [AddSNLTask()]
+            snl_priority = fw_spec.get('priority', 1)
+            spec = {'task_type': 'Add Deformed Struct to SNL database', 'snl': snl.to_dict, '_queueadapter': QA_DB, '_priority': snl_priority}
+            if 'snlgroup_id' in fw_spec and isinstance(snl, MPStructureNL):
+                spec['force_mpsnl'] = snl.to_dict
+                spec['force_snlgroup_id'] = fw_spec['snlgroup_id']
+                del spec['snl']
+            fws.append(FireWork(tasks, spec, name=get_slug(f + '--' + spec['task_type']), fw_id=-1000+i*10))
+            connections[-1000+i*10] = [-999+i*10]
+
             spec = snl_to_wf._snl_to_spec(snl)
             spec = update_spec_force_convergence(spec)
-            spec['vasp']['poscar'] = Poscar(d_struct)
-            spec['task_type'] = "Vasp deformed structure"
+
+            spec['task_type'] = "Calculate deformed structure"
             fws.append(FireWork([VaspWriterTask(), SetupElastConstTask(),
-                                 get_custodian_task(spec)], fw_spec, name=get_slug(f + '--' + fw_spec['task_type']), fw_id=-1000+i*2))
+                                 get_custodian_task(spec)], spec, name=get_slug(f + '--' + fw_spec['task_type']), fw_id=-999+i*10))
 
             priority = fw_spec['_priority']
             spec = {'task_type': 'VASP db insertion', '_priority': priority,
             '_allow_fizzled_parents': True, '_queueadapter': QA_DB}
-            fws.append(FireWork([VaspToDBTask()], spec, name=get_slug(f + '--' + fw_spec['task_type']), fw_id=-999+i*2))
-            connections[-1000+i*2] = [-999+i*2]
+            fws.append(FireWork([VaspToDBTask()], spec, name=get_slug(f + '--' + spec['task_type']), fw_id=-999+i*2))
+            connections[-999+i*10] = [-998+i*10]
 
             wf = Workflow(fws, connections)
             return FWAction(additions=wf)
         return FWAction()
+
