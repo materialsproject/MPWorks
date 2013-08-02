9c63549c5093f14262d4fbf5a279498d6e36f2f1
Anubhav Jain <ajain@lbl.gov>
2013-08-01 23:07:26 -0700

    add more clear functions

--- mpworks/fix_scripts/clear_FWs.py
+++ mpworks/fix_scripts/clear_FWs.py
@@ -21,10 +21,19 @@ class ClearFWs():
         self.lp.archive_wf(fw_id)
         return True
 
+    def defuse_fw(self, fw_id):
+        self.lp.defuse_fw(fw_id)
+        return True
+
 
 def _archive_fw(data):
     b = ClearFWs()
     return b.archive_fw(data)
+
+
+def _defuse_fw(data):
+    b = ClearFWs()
+    return b.defuse_fw(data)
 
 if __name__ == '__main__':
 
@@ -32,9 +41,19 @@ if __name__ == '__main__':
     cfw.setup()
     lp = ClearFWs.lp
     fw_ids = []
+
+    # archive READY WORKFLOWS
     for d in lp.workflows.find({"state": "READY"}, {'nodes': 1}):
         fw_ids.append(d['nodes'][0])
     print 'GOT all fw_ids...'
     pool = multiprocessing.Pool(8)
     states = pool.map(_archive_fw, fw_ids)
+    print 'DONE', all(states)
+
+    # defuse any READY/WAITING FWs
+    for d in lp.fireworks.find({"state": {"$in":["READY", "WAITING"]}}, {'fw_id': 1}):
+        fw_ids.append(d['fw_id'])
+    print 'GOT all fw_ids...'
+    pool = multiprocessing.Pool(8)
+    states = pool.map(_defuse_fw, fw_ids)
     print 'DONE', all(states)
