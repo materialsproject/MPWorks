1aa8df55b4a40ff64d2610de6ea4b9eff5450aab
Anubhav Jain <ajain@lbl.gov>
2013-10-03 17:36:31 -0700

    more example refinements

--- mpworks/examples/firetasks_ex.py
+++ mpworks/examples/firetasks_ex.py
@@ -1,10 +1,13 @@
+import json
 import os
 import shlex
 import socket
 from custodian import Custodian
 from custodian.vasp.jobs import VaspJob
 from fireworks.core.firework import FireTaskBase, FWAction
+from fireworks.core.launchpad import LaunchPad
 from fireworks.utilities.fw_serializers import FWSerializable
+from mpworks.drones.mp_vaspdrone import MPVaspDrone
 from pymatgen import PMGJSONDecoder
 
 __author__ = 'Anubhav Jain'
@@ -60,107 +63,32 @@ class VaspToDBTaskEx(FireTaskBase, FWSerializable):
 
     _fw_name = "Vasp to Database Task (Example)"
 
-    def __init__(self, parameters=None):
-        """
-        :param parameters: (dict) Potential keys are 'additional_fields', and 'update_duplicates'
-        """
-        parameters = parameters if parameters else {}
-        self.update(parameters)
-
-        self.additional_fields = self.get('additional_fields', {})
-        self.update_duplicates = self.get('update_duplicates', False)
 
     def run_task(self, fw_spec):
-            prev_dir = fw_spec['prev_vasp_dir']
-            update_spec = {'prev_vasp_dir': prev_dir,
-                           'prev_task_type': fw_spec['prev_task_type']}
-            self.additional_fields['run_tags'] = fw_spec['run_tags']
-            fizzled_parent = False
-            parse_dos = 'Uniform' in fw_spec['prev_task_type']
+        prev_dir = fw_spec['prev_vasp_dir']
 
-        if MOVE_TO_GARDEN_DEV:
-            prev_dir = move_to_garden(prev_dir, prod=False)
-
-        elif MOVE_TO_GARDEN_PROD:
-            prev_dir = move_to_garden(prev_dir, prod=True)
-
-        # get the directory containing the db file
+        # get the db credentials
         db_dir = os.environ['DB_LOC']
         db_path = os.path.join(db_dir, 'tasks_db.json')
 
-        logging.basicConfig(level=logging.INFO)
-        logger = logging.getLogger('MPVaspDrone')
-        logger.setLevel(logging.INFO)
-        sh = logging.StreamHandler(stream=sys.stdout)
-        sh.setLevel(getattr(logging, 'INFO'))
-        logger.addHandler(sh)
-
+        # use MPDrone to put it in the database
         with open(db_path) as f:
             db_creds = json.load(f)
             drone = MPVaspDrone(
                 host=db_creds['host'], port=db_creds['port'],
                 database=db_creds['database'], user=db_creds['admin_user'],
                 password=db_creds['admin_password'],
-                collection=db_creds['collection'], parse_dos=parse_dos,
-                additional_fields=self.additional_fields,
-                update_duplicates=self.update_duplicates)
+                collection=db_creds['collection'])
             t_id, d = drone.assimilate(prev_dir, launches_coll=LaunchPad.auto_load().launches)
 
-        mpsnl = d['snl_final'] if 'snl_final' in d else d['snl']
-        snlgroup_id = d['snlgroup_id_final'] if 'snlgroup_id_final' in d else d['snlgroup_id']
-        update_spec.update({'mpsnl': mpsnl, 'snlgroup_id': snlgroup_id})
-
+        # return FWAction
         print 'ENTERED task id:', t_id
         stored_data = {'task_id': t_id}
+        update_spec = {'prev_vasp_dir': prev_dir, 'prev_task_type': fw_spec['prev_task_type']}
         if d['state'] == 'successful':
             update_spec['analysis'] = d['analysis']
             update_spec['output'] = d['output']
             return FWAction(stored_data=stored_data, update_spec=update_spec)
 
-        # not successful - first test to see if UnconvergedHandler is needed
-        if not fizzled_parent:
-            unconverged_tag = 'unconverged_handler--{}'.format(fw_spec['prev_task_type'])
-            output_dir = last_relax(os.path.join(prev_dir, 'vasprun.xml'))
-            ueh = UnconvergedErrorHandler(output_filename=output_dir)
-            if ueh.check() and unconverged_tag not in fw_spec['run_tags']:
-                print 'Unconverged run! Creating dynamic FW...'
-
-                spec = {'prev_vasp_dir': get_block_part(prev_dir),
-                        'prev_task_type': fw_spec['task_type'],
-                        'mpsnl': mpsnl, 'snlgroup_id': snlgroup_id,
-                        'task_type': fw_spec['prev_task_type'],
-                        'run_tags': list(fw_spec['run_tags']),
-                        '_dupefinder': DupeFinderVasp().to_dict(),
-                        '_priority': fw_spec['_priority']}
-
-                snl = StructureNL.from_dict(spec['mpsnl'])
-                spec['run_tags'].append(unconverged_tag)
-                spec['_queueadapter'] = QA_VASP
-
-                fws = []
-                connections = {}
-
-                f = Composition.from_formula(
-                    snl.structure.composition.reduced_formula).alphabetical_formula
-
-                fws.append(FireWork(
-                    [VaspCopyTask({'files': ['INCAR', 'KPOINTS', 'POSCAR', 'POTCAR', 'CONTCAR'],
-                                   'use_CONTCAR': False}), SetupUnconvergedHandlerTask(),
-                     get_custodian_task(spec)], spec, name=get_slug(f + '--' + spec['task_type']),
-                    fw_id=-2))
-
-                spec = {'task_type': 'VASP db insertion', '_allow_fizzled_parents': True,
-                        '_priority': fw_spec['_priority'], '_queueadapter': QA_DB,
-                        'run_tags': list(fw_spec['run_tags'])}
-                spec['run_tags'].append(unconverged_tag)
-                fws.append(
-                    FireWork([VaspToDBTask()], spec, name=get_slug(f + '--' + spec['task_type']),
-                             fw_id=-1))
-                connections[-2] = -1
-
-                wf = Workflow(fws, connections)
-
-                return FWAction(detours=wf)
-
-        # not successful and not due to convergence problem - FIZZLE
-        raise ValueError("DB insertion successful, but don't know how to fix this FireWork! Can't continue with workflow...")
+        else:
+            raise ValueError("DB insertion successful, but don't know how to fix this FireWork! Can't continue with workflow...")
--- mpworks/examples/wf_ex.py
+++ mpworks/examples/wf_ex.py
@@ -3,8 +3,9 @@ from custodian.vasp.handlers import VaspErrorHandler, FrozenJobErrorHandler, Mes
 from custodian.vasp.jobs import VaspJob
 from fireworks.core.firework import FireWork, Workflow
 from fireworks.utilities.fw_utilities import get_slug
-from mpworks.examples.firetasks_ex import VaspCustodianTaskEx
-from mpworks.firetasks.vasp_io_tasks import VaspWriterTask
+from mpworks.examples.firetasks_ex import VaspCustodianTaskEx, VaspToDBTaskEx
+from mpworks.firetasks.vasp_io_tasks import VaspWriterTask, VaspCopyTask
+from mpworks.firetasks.vasp_setup_tasks import SetupStaticRunTask
 from pymatgen import Composition
 from pymatgen.io.vaspio_set import MPGGAVaspInputSet
 
@@ -15,8 +16,10 @@ __maintainer__ = 'Anubhav Jain'
 __email__ = 'ajain@lbl.gov'
 __date__ = 'Oct 03, 2013'
 
+
 def get_name(structure, task_type):
     return get_slug(structure.formula + '--' + task_type)
+
 
 def structure_to_wf(structure):
     """
@@ -25,17 +28,17 @@ def structure_to_wf(structure):
     :param structure:
     :return:
     """
-    fws = []
-    connections = defaultdict(list)
+    fws = []  # list of FireWorks to run
+    connections = defaultdict(list)  # dependencies between FireWorks
 
-    # generate VASP input objects for the structure
+    # generate VASP input objects for 1st VASP run - this is put in the FW spec
     mpvis = MPGGAVaspInputSet(user_incar_settings={'NPAR': 2})
     incar = mpvis.get_incar(structure)
     poscar = mpvis.get_poscar(structure)
     kpoints = mpvis.get_kpoints(structure)
     potcar = mpvis.get_potcar(structure)
 
-    # serialize the VASP output objects to the FW spec
+    # serialize the VASP input objects to the FW spec
     spec = {}
     spec['vasp'] = {}
     spec['vasp']['incar'] = incar.to_dict
@@ -45,23 +48,23 @@ def structure_to_wf(structure):
     spec['vaspinputset_name'] = mpvis.__class__.__name__
     spec['task_type'] = 'GGA optimize structure (2x) example'
 
-    # 1st FireWork - run GGA optimize structure
-    # VaspWriterTask - write input files (INCAR, POSCAR, KPOINTS, POSCAR) based on spec
-    # CustodianTaskEx - run VASP within a custodian
-
+    # set up the custodian that we want to run
     jobs = VaspJob.double_relaxation_run('', gzipped=False)
     handlers = [VaspErrorHandler(), FrozenJobErrorHandler(), MeshSymmetryErrorHandler(),
                     NonConvergingErrorHandler()]
-
     c_params = {'jobs': jobs, 'handlers': handlers, 'max_errors': 10}
+    custodiantask = VaspCustodianTaskEx(c_params)
 
-    tasks = [VaspWriterTask(), VaspCustodianTaskEx(c_params)]
+    # 1st FireWork - run GGA optimize structure
+    # VaspWriterTask - write input files (INCAR, POSCAR, KPOINTS, POSCAR) based on spec
+    # CustodianTaskEx - run VASP within a custodian
+    tasks = [VaspWriterTask(), custodiantask]
     fws.append(FireWork(tasks, spec, name=get_name(structure, spec['task_type']), fw_id=1))
 
     # 2nd FireWork - insert previous run into DB
     spec = {'task_type': 'VASP db insertion example'}
     fws.append(
-        FireWork([VaspToDBTask()], spec, name=get_name(structure, spec['task_type']), fw_id=2))
+        FireWork([VaspToDBTaskEx()], spec, name=get_name(structure, spec['task_type']), fw_id=2))
     connections[1] = [2]
 
     # 3rd FireWork - static run.
@@ -69,13 +72,16 @@ def structure_to_wf(structure):
     # SetupStaticRunTask - override old parameters for static run
     # CustodianTaskEx - run VASP within a custodian
     spec = {'task_type': 'GGA static example'}
-    fws.append(FireWork([VaspCopyTask({'use_CONTCAR': True, 'skip_CHGCAR': True}), SetupStaticRunTask(),get_custodian_task(spec)], spec, name=get_name(structure, spec['task_type']), fw_id=3))
+    copytask = VaspCopyTask({'use_CONTCAR': True, 'skip_CHGCAR': True})
+    setuptask = SetupStaticRunTask()
+    custodiantask = VaspCustodianTaskEx({'jobs': [VaspJob('')], 'handlers': handlers, 'max_errors': 10})
+    fws.append(FireWork([copytask, setuptask, custodiantask], spec, name=get_name(structure, spec['task_type']), fw_id=3))
     connections[2] = [3]
 
     # 4th FireWork - insert previous run into DB
     spec = {'task_type': 'VASP db insertion example'}
     fws.append(
-        FireWork([VaspToDBTask()], spec, name=get_name(structure, spec['task_type']), fw_id=4))
+        FireWork([VaspToDBTaskEx()], spec, name=get_name(structure, spec['task_type']), fw_id=4))
     connections[3] = [4]
 
     return Workflow(fws, connections, name=get_slug(structure.formula))
