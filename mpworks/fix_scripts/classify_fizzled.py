4981ae2ae5b9722144427a7e84b4ef0a7f4f7571
Anubhav Jain <ajain@lbl.gov>
2013-11-27 10:15:26 -0800

    classify_fizzled prints the launch_dir of jobs that failed due to "can't fix" error

--- mpworks/fix_scripts/classify_fizzled.py
+++ mpworks/fix_scripts/classify_fizzled.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     for f in lpdb.fireworks.find({"state": "FIZZLED"}, {'fw_id': 1}):
         fizzled_fws.append(f['fw_id'])
 
-    for l in lpdb.launches.find({"state": "FIZZLED", "action":{"$ne": None}}, {"action":1, 'fw_id': 1, 'time_start': 1}):
+    for l in lpdb.launches.find({"state": "FIZZLED", "action":{"$ne": None}}, {"action":1, 'fw_id': 1, 'time_start': 1, 'launch_dir':1}):
 
         if l['fw_id'] in fizzled_fws:
             except_str = l['action']['stored_data'].get('_exception')
@@ -44,12 +44,11 @@ if __name__ == '__main__':
                 except_dict['MEMORY_ERROR'] = except_dict['MEMORY_ERROR']+1
             elif 'DB insertion successful, but don\'t know how to fix' in except_str:
                 except_dict['NO_FIX'] = except_dict['NO_FIX']+1
+                print l['launch_dir']
             elif 'Poscar.from_string' in except_str and 'chunks[0]' in except_str:
                 except_dict['POSCAR_PARSE'] = except_dict['POSCAR_PARSE']+1
             elif 'TypeError: integer argument expected, got float':
                 except_dict['MAXRUN_TYPE'] = except_dict['MAXRUN_TYPE']+1
-                print l['fw_id']
-
             else:
                 except_dict[except_str] = except_dict[except_str]+1
 
