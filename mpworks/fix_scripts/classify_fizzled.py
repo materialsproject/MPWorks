ee58ae28b636964e0f31735edf56916365d9fc7d
Anubhav Jain <ajain@lbl.gov>
2014-01-23 11:32:31 -0800

    update classify_fizzled to be able to get parent launch loc

--- mpworks/fix_scripts/classify_fizzled.py
+++ mpworks/fix_scripts/classify_fizzled.py
@@ -9,6 +9,19 @@ __maintainer__ = 'Anubhav Jain'
 __email__ = 'ajain@lbl.gov'
 __date__ = 'Nov 11, 2013'
 
+def get_parent_launch_locs(fw_id, lpdb):
+    parent_fw_id = lpdb.workflows.find_one({"nodes": fw_id}, {"parent_links":1})['parent_links'][str(fw_id)][0]
+    print parent_fw_id
+    launch_ids = lpdb.fireworks.find_one({"fw_id": parent_fw_id},{'launches': 1})['launches']
+
+    locs = []
+    for l in launch_ids:
+        launch_loc = str(lpdb.launches.find_one({"launch_id": l}, {'launch_dir': 1})['launch_dir'])
+
+        locs.append("/project/projectdirs/matgen/garden/"+launch_loc[launch_loc.find('block_'):])
+
+    return locs
+
 if __name__ == '__main__':
     module_dir = os.path.dirname(os.path.abspath(__file__))
     lp_f = os.path.join(module_dir, 'my_launchpad.yaml')
@@ -21,12 +34,12 @@ if __name__ == '__main__':
         fizzled_fws.append(f['fw_id'])
 
     for l in lpdb.launches.find({"state": "FIZZLED", "action":{"$ne": None}}, {"action":1, 'fw_id': 1, 'time_start': 1, 'launch_dir':1}, timeout=False):
-
         if l['fw_id'] in fizzled_fws:
-            print '1'
             except_str = l['action']['stored_data'].get('_exception')
             if 'Disk quota exceeded' in except_str:
                  except_dict['DISK_QUOTA_EXCEEDED'] = except_dict['DISK_QUOTA_EXCEEDED']+1
+                 # print l['fw_id'], '*'
+                 # lpdb.rerun_fw(l['fw_id'])
             elif 'No such file' in except_str:
                 # this is due to missing CHGCAR from Michael's old runs
                 except_dict['NO_SUCH_FILE'] = except_dict['NO_SUCH_FILE']+1
@@ -44,6 +57,8 @@ if __name__ == '__main__':
                 except_dict['MEMORY_ERROR'] = except_dict['MEMORY_ERROR']+1
             elif 'DB insertion successful, but don\'t know how to fix' in except_str:
                 except_dict['NO_FIX'] = except_dict['NO_FIX']+1
+                print l['fw_id']
+                print get_parent_launch_locs(l['fw_id'], lpdb)
             elif 'Poscar.from_string' in except_str and 'chunks[0]' in except_str:
                 except_dict['POSCAR_PARSE'] = except_dict['POSCAR_PARSE']+1
             elif 'TypeError: integer argument expected, got float' in except_str:
@@ -54,10 +69,10 @@ if __name__ == '__main__':
                 except_dict['JINJA2'] = except_dict['JINJA2']+1
             elif 'run_tags' in except_str:
                 except_dict['RUN_TAGS'] = except_dict['RUN_TAGS']+1
-                #lpdb.rerun_fw(l['fw_id'])
             else:
                 except_dict[except_str] = except_dict[except_str]+1
 
     print '-----'
     for k, v in except_dict.iteritems():
         print {"{}\t{}".format(v, k)}
+
