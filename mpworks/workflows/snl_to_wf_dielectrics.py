427419eda6b1a481c36b67fe234fa197887e2db6
petousis <ioannis.petousis@gmail.com>
2014-08-20 14:04:21 -0700

    Added author to nsl_to_wf_dielectrics.py and removed dielectrics_tasks.py

--- mpworks/workflows/snl_to_wf_dielectrics.py
+++ mpworks/workflows/snl_to_wf_dielectrics.py
@@ -1,6 +1,8 @@
 from fireworks.core.firework import FireWork
 from mpworks.firetasks.vasp_io_tasks import VaspCopyTask, VaspWriterTask, VaspToDBTask
 
+__author__ = 'Ioannis Petousis'
+
 from fireworks.utilities.fw_utilities import get_slug
 from mpworks.firetasks.custodian_task import get_custodian_task
 from mpworks.snl_utils.mpsnl import get_meta_from_structure, MPStructureNL
