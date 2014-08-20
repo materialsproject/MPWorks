6851e71cfdd37e5e520c772ede5757368c11e61d
petousis <ioannis.petousis@gmail.com>
2014-08-20 15:31:24 -0700

    Changed formatting

--- mpworks/workflows/snl_to_wf_dielectrics.py
+++ mpworks/workflows/snl_to_wf_dielectrics.py
@@ -31,7 +31,7 @@ def snl_to_wf_static_dielectrics(snl, parameters=None):
     
     # add the SNL to the SNL DB and figure out duplicate group
     tasks = [AddSNLTask()]
-	spec = {'task_type': 'Add to SNL database', 'snl': snl.to_dict, '_queueadapter': QA_DB, '_priority': snl_priority}
+    spec = {'task_type': 'Add to SNL database', 'snl': snl.to_dict, '_queueadapter': QA_DB, '_priority': snl_priority}
     if 'snlgroup_id' in parameters and isinstance(snl, MPStructureNL):
         spec['static_dielectrics_mpsnl'] = snl.to_dict
         spec['static_dielectrics_snlgroup_id'] = parameters['snlgroup_id']
@@ -59,10 +59,8 @@ def snl_to_wf_static_dielectrics(snl, parameters=None):
     connections[0] = [1] # define fw_id=1 is dependent on completion of fw_id=0
 
     # insert into DB - GGA structure optimization
-    spec = {'task_type': 'VASP db insertion', '_priority': priority,
-            '_allow_fizzled_parents': True, '_queueadapter': QA_DB}
-    fws.append(
-        FireWork([VaspToDBTask()], spec, name=get_slug(f + '--' + spec['task_type']), fw_id=2))
+    spec = {'task_type': 'VASP db insertion', '_priority': priority, '_allow_fizzled_parents': True, '_queueadapter': QA_DB}
+    fws.append(FireWork([VaspToDBTask()], spec, name=get_slug(f + '--' + spec['task_type']), fw_id=2))
     connections[1] = [2] # define fw_id=2 is dependent on completion of fw_id=1
 
     wf_meta = get_meta_from_structure(snl.structure)
@@ -71,5 +69,4 @@ def snl_to_wf_static_dielectrics(snl, parameters=None):
     if '_materialsproject' in snl.data and 'submission_id' in snl.data['_materialsproject']:
         wf_meta['submission_id'] = snl.data['_materialsproject']['submission_id']
 
-    return Workflow(fws, connections, name=Composition.from_formula(
-        snl.structure.composition.reduced_formula).alphabetical_formula, metadata=wf_meta)
+    return Workflow(fws, connections, name=Composition.from_formula(snl.structure.composition.reduced_formula).alphabetical_formula, metadata=wf_meta)
