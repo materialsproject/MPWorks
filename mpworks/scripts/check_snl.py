2cd98cc2d182e1da2fe0b630a1bc2ecc801de7d3
Patrick Huck <patrick@the-huck.com>
2014-09-24 16:03:27 -0700

    check_snl: back to single-value dicts for stream write

--- mpworks/scripts/check_snl.py
+++ mpworks/scripts/check_snl.py
@@ -43,25 +43,13 @@ def check_snl_spacegroups(args):
     mpsnl_cursor = sma.snl.find({ "snl_id": id_range})
     s = py.Stream(stream_ids[0]) # keep open in HPC env
     s.open()
-    data = dict(x=[], y=[])
-    start_time = time.clock()
     for mpsnl_dict in mpsnl_cursor:
         mpsnl = MPStructureNL.from_dict(mpsnl_dict)
         sf = SymmetryFinder(mpsnl.structure, symprec=0.1)
         if sf.get_spacegroup_number() == mpsnl.sg_num:
-            data['x'].append(mpsnl_dict['snl_id'])
-            data['y'].append(1)
-            time_diff = time.clock() - start_time
-            if time_diff > 0.08:
-                print data
-                s.write(data)
-                data['x'][:] = []
-                data['y'][:] = []
-                start_time = time.clock()
-    sleep_time =  0.08 - time.clock() + start_time
-    time.sleep(sleep_time)
-    print data
-    s.write(data)
+            data = dict(x=mpsnl_dict['snl_id'], y=1)
+            s.write(data)
+            time.sleep(0.08)
     s.close()
 
 def check_snls_in_snlgroups(args):
