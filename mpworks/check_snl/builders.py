41b4f2955a3d949020ec7c092732f3344d8304cd
Patrick Huck <patrick@the-huck.com>
2014-10-02 00:23:05 -0700

    check_snl: matcher and _log in builder

--- mpworks/check_snl/builders.py
+++ mpworks/check_snl/builders.py
@@ -1,8 +1,18 @@
 from mpworks.snl_utils.mpsnl import MPStructureNL, SNLGroup
+from pymatgen.analysis.structure_matcher import StructureMatcher, ElementComparator
 from matgendb.builders.core import Builder
+from matgendb.builders.util import get_builder_log
+
+_log = get_builder_log("cross_checker")
 
 class SNLGroupCrossChecker(Builder):
     """cross-check all SNL Groups via StructureMatcher.fit of their canonical SNLs"""
+    def __init__(self, *args, **kwargs):
+        self._matcher = StructureMatcher(
+            ltol=0.2, stol=0.3, angle_tol=5, primitive_cell=True, scale=True,
+            attempt_supercell=False, comparator=ElementComparator()
+        )
+        Builder.__init__(self, *args, **kwargs)
 
     def get_items(self, snlgroups=None):
         """iterator over same-composition groups of SNLGroups rev-sorted by size
@@ -33,7 +43,18 @@ class SNLGroupCrossChecker(Builder):
                 snlgroups[gid] = SNLGroup.from_dict(snlgrp_dict)
             return snlgroups[gid]
 
-        for snlgroup_id in item['snlgroup_ids']:
-            print snlgroup_id, _get_snl_group(snlgroup_id)
-        print snlgroups.keys()
+        for idx,primary_id in enumerate(item['snlgroup_ids'][:-1]):
+            primary_group = _get_snl_group(primary_id)
+            for secondary_id in item['snlgroup_ids'][idx+1:]:
+                secondary_group = _get_snl_group(secondary_id)
+                is_match = self._matcher.fit(
+                    primary_group.canonical_structure,
+                    secondary_group.canonical_structure
+                )
+                _log.info('%d:%s, %d:%s = %r' % (
+                    primary_id, primary_group.canonical_snl.snlgroup_key,
+                    secondary_id, secondary_group.canonical_snl.snlgroup_key,
+                    is_match
+                ))
+        _log.info(snlgroups.keys())
 
