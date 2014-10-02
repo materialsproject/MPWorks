c5870c263c803433ad672552496f5e4bbc278b85
Patrick Huck <patrick@the-huck.com>
2014-10-01 23:54:17 -0700

    check_snl: implementing process_item for SNLGroupCrossChecker builder

--- mpworks/check_snl/builders.py
+++ mpworks/check_snl/builders.py
@@ -1,7 +1,9 @@
+from mpworks.snl_utils.mpsnl import MPStructureNL, SNLGroup
 from matgendb.builders.core import Builder
 
 class SNLGroupCrossChecker(Builder):
     """cross-check all SNL Groups via StructureMatcher.fit of their canonical SNLs"""
+
     def get_items(self, snlgroups=None):
         """iterator over same-composition groups of SNLGroups rev-sorted by size
 
@@ -9,17 +11,29 @@ class SNLGroupCrossChecker(Builder):
         :type snlgroups: QueryEngine
         """
         self._snlgroups = snlgroups
-        pipeline = [ { '$limit': 100 } ]
+        pipeline = [ { '$limit': 1000 } ]
         group_expression = {
             '_id': '$reduced_cell_formula_abc',
             'num_snlgroups': { '$sum': 1 },
-            'snl_groupids': { '$addToSet': "$snlgroup_id" }
+            'snlgroup_ids': { '$addToSet': "$snlgroup_id" }
         }
         pipeline.append({ '$group': group_expression })
         pipeline.append({ '$sort': { 'num_snlgroups': -1 } })
         pipeline.append({ '$project': { 'snlgroup_ids': 1 } })
         return self._snlgroups.collection.aggregate(pipeline)['result']
-    def process_item(self, item):
-        print item
 
+    def process_item(self, item):
+        """combine all SNL Groups for current composition (item)"""
+        if len(item['snlgroup_ids']) < 2: return 0
+        snlgroups = {}
+
+        def _get_snl_group(gid):
+            if gid not in snlgroups:
+                snlgrp_dict = self._snlgroups.collection.find_one({ "snlgroup_id": gid })
+                snlgroups[gid] = SNLGroup.from_dict(snlgrp_dict)
+            return snlgroups[gid]
+
+        for snlgroup_id in item['snlgroup_ids']:
+            print snlgroup_id, _get_snl_group(snlgroup_id)
+        print snlgroups.keys()
 
