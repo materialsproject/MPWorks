52d255e2809bd24fd4cb3663dfc4c969a34f8cb6
Anubhav Jain <ajain@lbl.gov>
2015-02-05 08:41:45 -0800

    boltztrap updates, requires MPCollab install

--- mpworks/firetasks/boltztrap_tasks.py
+++ mpworks/firetasks/boltztrap_tasks.py
@@ -1,5 +1,6 @@
 from __future__ import division
 from collections import defaultdict
+import copy
 import gridfs
 import time
 from fireworks import FireTaskBase
@@ -8,14 +9,13 @@ import os
 from pymongo import MongoClient
 from fireworks.core.firework import FWAction
 from fireworks.utilities.fw_serializers import FWSerializable
-from monty.os.path import zpath
 from monty.json import jsanitize, MontyEncoder
 from mpworks.snl_utils.mpsnl import get_meta_from_structure
-from mpworks.workflows.wf_utils import get_loc, get_block_part
-from pymatgen import Structure
+from mpworks.workflows.wf_utils import get_block_part
+import mpcollab.thermoelectrics.boltztrap_TE as bzte
+import numpy as np
 from pymatgen.electronic_structure.bandstructure import BandStructure
 from pymatgen.electronic_structure.boltztrap import BoltztrapRunner, BoltztrapAnalyzer
-from pymatgen.io.vaspio.vasp_output import Vasprun
 
 __author__ = 'Geoffroy Hautier, Anubhav Jain'
 __copyright__ = 'Copyright 2014, The Materials Project'
@@ -27,6 +27,8 @@ __date__ = 'Feb 24, 2014'
 
 class BoltztrapRunTask(FireTaskBase, FWSerializable):
     _fw_name = "Boltztrap Run Task"
+    TAU = 1E-14
+    KAPPAL = 1
 
     def generate_te_doping(self, d):
         types = ['p', 'n']
@@ -56,37 +58,61 @@ class BoltztrapRunTask(FireTaskBase, FWSerializable):
 
         return pf_dict, zt_dict
 
-    def get_extreme(self, d, target, maximize=True, types=('p', 'n'), iso_cutoff=0.05):
-        """
+    def get_eigs(d, target, iso_cutoff=0.05):
+        eigs_d = copy.deepcopy(d[target])
+        for te_type in ('p', 'n'):
+                for t in eigs_d[te_type]:  # temperatures
+                    for didx, tensor in enumerate(eigs_d[te_type][t]):  # doping idx
+                        eigs = np.linalg.eigh(tensor)[0].tolist()
+                        st = sorted(eigs)
+                        st = [round(e, 2) for e in st]
+                        if [st[0],st[1],st[2]] == [0, 0, 0]:
+                            isotropic = True
+                        else:
+                            isotropic = all([st[0],st[1],st[2]]) and (abs((st[1]-st[0])/st[1]) <= iso_cutoff) and (abs((st[2]-st[0]))/st[2] <= iso_cutoff) and (abs((st[2]-st[1])/st[2]) <= iso_cutoff)
+                        eigs_d[te_type][t][didx] = {'eigs': eigs, 'isotropic': isotropic}
 
-        :param d: data dictionary
-        :param target: root key of target property, e.g. 'seebeck_doping'
-        :param maximize: (bool) max if True, min if False
-        :param types:
-        :param iso_cutoff: percent cutoff for isotropicity
-        :return:
-        """
-        max = None
-        max_type = None
-        max_temp = None
-        max_dope = None
-        max_mu = None
-        isotropic = None
+        return eigs_d
 
-        for type in types:
-            for t in d[target][type]:  # temperatures
-                for didx, tensor in enumerate(d[target][type][t]):  # doping idx
-                    for tidx, val in enumerate(tensor):
-                        if (val > max and maximize) or (val < max and not maximize) or max is None:
-                            max = val
-                            max_type = type
-                            max_temp = t
-                            max_dope = d['doping'][type][didx]
-                            max_mu = d['mu_doping'][type][didx]
-                            st = sorted(tensor)
-                            isotropic = all([st[0],st[1],st[2]]) and ((st[1]-st[0])/st[1] <= iso_cutoff) and ((st[2]-st[0])/st[2] <= iso_cutoff) and ((st[2]-st[1])/st[2] <= iso_cutoff)
 
-        return {'value': max, 'type': max_type, 'temperature': max_temp, 'doping': max_dope, 'mu': max_mu, 'isotropic': isotropic}
+    def get_extreme(d, target, maximize=True, iso_cutoff=0.05):
+            """
+
+            :param d: data dictionary
+            :param target: root key of target property, e.g. 'seebeck_doping'
+            :param maximize: (bool) max if True, min if False
+            :param iso_cutoff: percent cutoff for isotropicity
+            :return:
+            """
+            max_val = None
+            max_temp = None
+            max_dope = None
+            max_mu = None
+            isotropic = None
+            data = {}
+
+            for te_type in ('p', 'n'):
+                for t in d[target][te_type]:  # temperatures
+                    for didx, evs in enumerate(d[target][te_type][t]):  # doping idx
+                        for val in evs['eigs']:
+                            if (val > max_val and maximize) or (val < max_val and not maximize) or max_val is None:
+                                max_val = val
+                                max_temp = float(t)
+                                max_dope = d['doping'][te_type][didx]
+                                max_mu = d['mu_doping'][te_type][t][didx]
+
+                                isotropic = evs['isotropic']
+                data[te_type] = {'value': max_val, 'temperature': max_temp, 'doping': max_dope, 'mu': max_mu, 'isotropic': isotropic}
+                max_val = None
+
+            if maximize:
+                max_type = 'p' if data['p']['value'] >= data['n']['value'] else 'n'
+            else:
+                max_type = 'p' if data['p']['value'] <= data['n']['value'] else 'n'
+
+            data['best'] = data[max_type]
+            data['best']['type'] = max_type
+            return data
 
     def run_task(self, fw_spec):
 
@@ -145,30 +171,45 @@ class BoltztrapRunTask(FireTaskBase, FWSerializable):
             data['dir_name'] = get_block_part(dir)
             data['task_id'] = m_task['task_id']
             del data['hall']  # remove because it is too large and not useful
-            del data['hall_doping']  # remove because it is too large and not useful
             fs = gridfs.GridFS(tdb, "boltztrap_full")
             btid = fs.put(json.dumps(jsanitize(data)))
 
             # now for the "sanitized" data
-            data['boltztrap_full_fs_id'] = btid
-            # kill the super-long documents vs. mu
-            to_delete = ['cond', 'seebeck', 'kappa', 'carrier_conc']
-            for k in to_delete:
-                del data[k]
+            te_analyzer = bzte.BoltztrapAnalyzerTE.from_BoltztrapAnalyzer(bta)
 
-            # add TE properties
-            data['PF_doping'], data['ZT_doping'] = self.generate_te_doping(data)
+            ted = te_analyzer.as_dict()
+            del ted['seebeck']
+            del ted['hall']
+            del ted['kappa']
+            del ted['cond']
 
-            # add maximums
-            data['best_seebeck'] = self.get_extreme(data, 'seebeck_doping', maximize=True)
-            data['best_cond'] = self.get_extreme(data, 'cond_doping', maximize=True)
-            data['best_kappa'] = self.get_extreme(data, 'kappa_doping', maximize=False)
-            data['best_PF'] = self.get_extreme(data, 'PF_doping', maximize=True)
-            data['best_ZT'] = self.get_extreme(data, 'ZT_doping', maximize=True)
+            ted['boltztrap_full_fs_id'] = btid
+            ted['snlgroup_id'] = fw_spec['snlgroup_id']
+            ted['run_tags'] = fw_spec['run_tags']
+            ted['snl'] = fw_spec['mpsnl']
+            ted['dir_name_full'] = dir
+            ted['dir_name'] = get_block_part(dir)
+            ted['task_id'] = m_task['task_id']
 
-            tdb.boltztrap.insert(jsanitize(data))
+            ted['pf_doping'] = te_analyzer.get_power_factor(tau=self.TAU).as_dict()
+            ted['zt_doping'] = te_analyzer.get_ZT(kappal=self.KAPPAL, tau=self.TAU).as_dict()
 
-        update_spec = {'prev_vasp_dir': fw_spec['prev_vasp_dir'],
+            ted['pf_eigs'] = self.get_eigs(ted, 'pf_doping')
+            ted['pf_best'] = self.get_extreme(ted, 'pf_eigs')
+            ted['zt_eigs'] = self.get_eigs(ted, 'zt_doping')
+            ted['zt_best'] = self.get_extreme(ted, 'zt_eigs')
+            ted['seebeck_eigs'] = self.get_eigs(ted, 'seebeck_doping')
+            ted['seebeck_best'] = self.get_extreme(ted, 'seebeck_eigs')
+            ted['cond_eigs'] = self.get_eigs(ted, 'cond_doping')
+            ted['cond_best'] = self.get_extreme(ted, 'cond_eigs')
+            ted['kappa_eigs'] = self.get_eigs(ted, 'kappa_doping')
+            ted['kappa_best'] = self.get_extreme(ted, 'kappa_eigs', maximize=False)
+            ted['hall_eigs'] = self.get_eigs(ted, 'hall_doping')
+            ted['hall_best'] = self.get_extreme(ted, 'hall_eigs')
+
+            tdb.boltztrap.insert(jsanitize(ted))
+
+            update_spec = {'prev_vasp_dir': fw_spec['prev_vasp_dir'],
                        'boltztrap_dir': os.getcwd(),
                        'prev_task_type': fw_spec['task_type'],
                        'mpsnl': fw_spec['mpsnl'],
