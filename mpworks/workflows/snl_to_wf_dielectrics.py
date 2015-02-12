e5f9cdd796d2441f507482cc0b5e97cd19a004ff
Ioannis Petousis <ioannis.petousis@gmail.com>
2015-02-11 16:25:54 -0800

    Changed comment

--- mpworks/workflows/snl_to_wf_dielectrics.py
+++ mpworks/workflows/snl_to_wf_dielectrics.py
@@ -30,14 +30,14 @@ def snl_to_wf_static_dielectrics(snl, parameters=None):
     
     # add the SNL to the SNL DB and figure out duplicate group
     tasks = [AddSNLTask()]
-    spec = {'task_type': 'Add to SNL database', 'snl': snl.as_dict(), '_queueadapter': QA_CONTROL, '_priority': snl_priority}
+    spec = {'task_type': 'Add to SNL database', 'snl': snl.as_dict(), '_queueadapter': QA_DB, '_priority': snl_priority}
     if 'snlgroup_id' in parameters and isinstance(snl, MPStructureNL):
         spec['static_dielectrics_mpsnl'] = snl.as_dict()
         spec['static_dielectrics_snlgroup_id'] = parameters['snlgroup_id']
         del spec['snl']
     fws.append(Firework(tasks, spec, name=get_slug(f + '--' + spec['task_type']), fw_id=0))
     
-    # run GGA structure optimization for static dielectric convergence
+    # run DFPT for static dielectrics run:
     spec = snl_to_wf._snl_to_spec(snl, parameters=parameters)
     mpvis = MPStaticDielectricDFPTVaspInputSet()
     incar = mpvis.get_incar(snl.structure)
@@ -58,7 +58,7 @@ def snl_to_wf_static_dielectrics(snl, parameters=None):
     connections[0] = [1] # define fw_id=1 is dependent on completion of fw_id=0
 
     # insert into DB - GGA structure optimization
-    spec = {'task_type': 'VASP db insertion', '_priority': priority, '_allow_fizzled_parents': True, '_queueadapter': QA_CONTROL}
+    spec = {'task_type': 'VASP db insertion', '_priority': priority, '_allow_fizzled_parents': True, '_queueadapter': QA_DB}
     fws.append(Firework([VaspToDBTask()], spec, name=get_slug(f + '--' + spec['task_type']), fw_id=2))
     connections[1] = [2] # define fw_id=2 is dependent on completion of fw_id=1
 
