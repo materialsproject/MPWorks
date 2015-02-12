d229659287a5ff1ae6e623879de4904e0f3103a3
Ioannis Petousis <ioannis.petousis@gmail.com>
2015-02-12 15:55:18 -0800

    Change to include force convergence run before the DFPT run

--- mpworks/workflows/snl_to_wf_dielectrics.py
+++ mpworks/workflows/snl_to_wf_dielectrics.py
@@ -8,7 +8,8 @@ from mpworks.snl_utils.mpsnl import get_meta_from_structure, MPStructureNL
 from mpworks.firetasks.snl_tasks import AddSNLTask
 from mpworks.workflows.wf_settings import QA_DB, QA_VASP, QA_CONTROL
 from pymatgen import Composition
-from pymatgen.io.vaspio_set import MPStaticDielectricDFPTVaspInputSet
+from pymatgen.core.structure import Structure
+from pymatgen.io.vaspio_set import MPStaticDielectricDFPTVaspInputSet, MPVaspInputSet
 from pymatgen.io.vaspio.vasp_input import Incar, Poscar, Kpoints
 from fireworks.core.firework import Firework, Workflow
 
@@ -37,10 +38,55 @@ def snl_to_wf_static_dielectrics(snl, parameters=None):
         del spec['snl']
     fws.append(Firework(tasks, spec, name=get_slug(f + '--' + spec['task_type']), fw_id=0))
     
+    predecessor = 0
+    successor = 1
+
+
+    if 'force_convergence' in snl.projects:
+        # run optmization with force as convergence criterion:
+        spec = snl_to_wf._snl_to_spec(snl, parameters=parameters)
+        mpvis = MPVaspInputSet()
+        incar = mpvis.get_incar(snl.structure)
+
+        incar.update({"EDIFF":parameters['EDIFF']})
+        incar.update({"EDIFFG":parameters['EDIFFG']})
+        incar.update({"ENCUT":parameters['ENCUT']})
+        spec['vasp']['incar'] = incar.as_dict()
+        kpoints_density = int(parameters['kpoint_density'])
+        k=Kpoints.automatic_density(snl.structure, kpoints_density)
+        spec['vasp']['kpoints'] = k.as_dict()
+        # spec = update_spec_static_dielectrics_convergence(spec)
+        del spec['_dupefinder']
+        # spec['run_tags'].append("origin")
+        spec['_priority'] = priority
+        spec['_queueadapter'] = QA_VASP
+        spec['task_type'] = "force convergence" # Change name here: delete Vasp? 
+        tasks = [VaspWriterTask(), get_custodian_task(spec)]
+        fws.append(Firework(tasks, spec, name=get_slug(f + '--' + spec['task_type']), fw_id=successor))
+        connections[predecessor] = [successor] # define fw_id=1 is dependent on completion of fw_id=0
+        predecessor += 1
+        successor += 1
+
+        # insert into DB - Force optimization
+        spec = {'task_type': 'VASP Force db insertion', '_priority': priority, '_allow_fizzled_parents': True, '_queueadapter': QA_DB}
+        fws.append(Firework([VaspToDBTask()], spec, name=get_slug(f + '--' + spec['task_type']), fw_id=successor))
+        connections[predecessor] = [successor] # define fw_id=2 is dependent on completion of fw_id=1
+        predecessor += 1
+        successor += 1
+
+
     # run DFPT for static dielectrics run:
+    if 'force_convergence' in snl.projects:
+        relaxed_structure = spec['output']['crystal']
+    
     spec = snl_to_wf._snl_to_spec(snl, parameters=parameters)
     mpvis = MPStaticDielectricDFPTVaspInputSet()
     incar = mpvis.get_incar(snl.structure)
+    
+    if 'force_convergence' in snl.projects:
+        spec['vasp']['poscar'] = relaxed_structure
+        #poscar = mpvis.get_poscar(Structure.from_dict(spec['output']['crystal']))
+    
     incar.update({"EDIFF":parameters['EDIFF']})
     incar.update({"ENCUT":parameters['ENCUT']})
     spec['vasp']['incar'] = incar.as_dict()
@@ -54,13 +100,15 @@ def snl_to_wf_static_dielectrics(snl, parameters=None):
     spec['_queueadapter'] = QA_VASP
     spec['task_type'] = "Static Dielectrics Calculation" # Change name here: delete Vasp? 
     tasks = [VaspWriterTask(), get_custodian_task(spec)]
-    fws.append(Firework(tasks, spec, name=get_slug(f + '--' + spec['task_type']), fw_id=1))
-    connections[0] = [1] # define fw_id=1 is dependent on completion of fw_id=0
+    fws.append(Firework(tasks, spec, name=get_slug(f + '--' + spec['task_type']), fw_id=successor))
+    connections[predecessor] = [successor] # define fw_id=1 is dependent on completion of fw_id=0
+    predecessor += 1
+    successor += 1
 
-    # insert into DB - GGA structure optimization
+    # insert into DB - Static Dielectrics run
     spec = {'task_type': 'VASP db insertion', '_priority': priority, '_allow_fizzled_parents': True, '_queueadapter': QA_DB}
-    fws.append(Firework([VaspToDBTask()], spec, name=get_slug(f + '--' + spec['task_type']), fw_id=2))
-    connections[1] = [2] # define fw_id=2 is dependent on completion of fw_id=1
+    fws.append(Firework([VaspToDBTask()], spec, name=get_slug(f + '--' + spec['task_type']), fw_id=successor))
+    connections[predecessor] = [successor] # define fw_id=2 is dependent on completion of fw_id=1
 
     wf_meta = get_meta_from_structure(snl.structure)
     wf_meta['run_version'] = 'May 2013 (1)'
