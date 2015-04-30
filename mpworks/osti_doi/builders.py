48d391e13253d53c19b60801797e8b226fb4392f
Patrick Huck <patrick@the-huck.com>
2015-04-29 19:40:08 -0700

    osti_doi: DoiValidator runs, testing todo

--- mpworks/osti_doi/builders.py
+++ mpworks/osti_doi/builders.py
@@ -15,23 +15,23 @@ class DoiValidator(Builder):
         :param materials: 'materials' collection in 'mg_core_dev/prod'
         :type materials: QueryEngine
         """
-        self.doicoll = dois
-        self.matcoll = materials
+        self.doi_qe = dois
+        self.mat_qe = materials
         self.headers = {'Accept': 'text/bibliography; style=bibtex'}
         # loop the mp-id's
         # w/o valid DOI in doicoll *OR*
         # w/ valid DOI in doicoll but w/o doi key in matcoll
         mp_ids = [
             {'_id': doc['_id'], 'doi': doc['doi'], 'valid': False}
-            for doc in self.doicoll.find({'valid': False})
+            for doc in self.doi_qe.collection.find({'valid': False})
         ]
-        valid_mp_ids = self.doicoll.find({'valid': True}).distinct('_id')
-        missing_mp_ids = self.matcoll.find(
+        valid_mp_ids = self.doi_qe.collection.find({'valid': True}).distinct('_id')
+        missing_mp_ids = self.mat_qe.collection.find(
             {'task_id': {'$in': valid_mp_ids}, 'doi': {'$exists': False}},
             {'_id': 0, 'task_id': 1}
         ).distinct('task_id')
-        mp_ids += list(self.doicoll.find(
-            {'_id': {'$in': missing_mp_ids}}
+        mp_ids += list(self.doi_qe.collection.find(
+            {'_id': {'$in': missing_mp_ids}},
             {'doi': 1, 'valid': 1, 'bibtex': 1}
         ))
         return mp_ids
@@ -42,19 +42,19 @@ class DoiValidator(Builder):
             doi_url = 'http://doi.org/{}'.format(item['doi'])
             r = requests.get(doi_url, headers=self.headers)
             if r.status_code == 200:
-                logger.info(self.doicoll.update(
+                logger.info(self.doi_qe.collection.update(
                     {'_id': item['_id']}, {'$set': {
                         'valid': True, 'bibtex': r.content
                     }}
                 ))
                 # only validated DOIs are ready to be built into matcoll
-                logger.info(self.matcoll.update(
+                logger.info(self.mat_qe.collection.update(
                     {'_id': item['_id']}, {'$set': {
                         'doi': item['doi'], 'doi_bibtex': r.content
                     }}
                 ))
         else:
-            logger.info(self.matcoll.update(
+            logger.info(self.mat_qe.collection.update(
                 {'_id': item['_id']}, {'$set': {
                     'doi': item['doi'], 'doi_bibtex': item['bibtex']
                 }}
