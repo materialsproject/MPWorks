ce5f8a6aeb65f4b7c747dece1d15226b95c36604
Patrick Huck <phuck@lbl.gov>
2015-05-14 17:42:00 -0700

    osti_doi: run script works

--- mpworks/osti_doi/run.sh
+++ mpworks/osti_doi/run.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
-
-mpworks=$HOME/MPWorks
-cd $mpworks
-workon env_mp_osti_doi
+source $HOME/.credentials
+source $HOME/.virtualenvs/env_mp_osti_doi/bin/activate
+cd $HOME/MPWorks
 export PYTHONPATH=`pwd`:$PYTHONPATH
 mgbuild run -v mpworks.osti_doi.builders.DoiBuilder nmats=1 dois=dois.json materials=materials.json
+git add mpworks/osti_doi/dois.json
+git commit -m "osti_doi: new dois backup"
