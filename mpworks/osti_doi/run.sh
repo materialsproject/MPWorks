42530002bd3b70e6d78de1792d1ce5223c1074af
Patrick Huck <phuck@lbl.gov>
2015-06-04 13:21:02 -0700

    osti_doi/run.sh: set nmats to 20

--- mpworks/osti_doi/run.sh
+++ mpworks/osti_doi/run.sh
@@ -2,7 +2,7 @@ source $HOME/.credentials
 source $HOME/.virtualenvs/env_mp_osti_doi/bin/activate
 cd $HOME/MPWorks
 export PYTHONPATH=`pwd`:$PYTHONPATH
-mgbuild run -v mpworks.osti_doi.builders.DoiBuilder nmats=10 dois=dois.json materials=materials.json
+mgbuild run -v mpworks.osti_doi.builders.DoiBuilder nmats=20 dois=dois.json materials=materials.json
 git add mpworks/osti_doi/dois.json
 git commit -m "osti_doi: new dois backup"
 git push origin osti_doi
