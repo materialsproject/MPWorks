eeae5b4f607b24479cdfa48142addcdd50b68cbf
Patrick Huck <phuck@lbl.gov>
2015-06-05 11:24:48 -0700

    minor wf_settings format revert

--- mpworks/workflows/wf_settings.py
+++ mpworks/workflows/wf_settings.py
@@ -5,13 +5,11 @@ __maintainer__ = 'Anubhav Jain'
 __email__ = 'ajain@lbl.gov'
 __date__ = 'May 10, 2013'
 
-# don't change nnodes unless other parts of code are also changed
-QA_VASP = {'nnodes': 2}
+QA_VASP = {'nnodes': 2}  # don't change nnodes unless other parts of code are also changed
 QA_VASP_SMALL = {'nnodes': 2, 'walltime': '72:00:00'}  # small walltime jobs
 QA_DB = {'nnodes': 1, 'walltime': '1:00:00',
          'pre_rocket': '#PBS -V\nulimit -v hard\nmodule load python/2.7.3\nsource $VENV_LOC'}
 QA_CONTROL = {'nnodes': 1, 'walltime': '00:30:00'}
-
 MOVE_TO_GARDEN_DEV = False
 MOVE_TO_GARDEN_PROD = False
 
