6504d4cbf53af5fd30b92b150458bcefe38e08ae
Richard <rit001@ucsd.edu>
2015-06-22 16:05:17 -0700

    typos

--- mpworks/firetasks/surface_tasks.py
+++ mpworks/firetasks/surface_tasks.py
@@ -122,7 +122,7 @@ class WriteSurfVaspInput(FireTaskBase):
 
         mplb_s = MPSlabVaspInputSet(user_incar_settings=user_incar_settings, k_product=k_product,
                                     potcar_functional=potcar_functional, bulk = False)
-        mplb_s.write_input(slab_cell, '%s_scell_k%s_%s' %(element, k_product, st(miller_index)))
+        mplb_s.write_input(slab_cell, '%s_scell_k%s_%s' %(element, k_product, str(miller_index)))
 
 
 
