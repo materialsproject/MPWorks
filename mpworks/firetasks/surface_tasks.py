53b10a67b85d1779b772a7959bb86b5d275e7794
Richard <rit001@ucsd.edu>
2015-07-15 22:29:59 -0700

    .

--- mpworks/firetasks/surface_tasks.py
+++ mpworks/firetasks/surface_tasks.py
@@ -249,30 +249,30 @@ class WriteSlabVaspInputs(FireTaskBase):
                                      'structure_type': 'oriented_unit_cell',
                                      'miller index': miller_index},
                                      optional_data=optional_data)
+        for entry in bulk_entry:
+            if entry.data['state'] != 'successful':
+                print "%s bulk calculations were incomplete, cancelling FW" \
+                      %(relax_orient_uc.composition.reduced_formula)
+                return FWAction()
+            else:
 
-        if bulk_entry.data['state'] != 'successful':
-            print "%s bulk calculations were incomplete, cancelling FW" \
-                  %(relax_orient_uc.composition.reduced_formula)
-            return FWAction()
-        else:
+                FWs = []
+                for slab in slab_list:
 
-            FWs = []
-            for slab in slab_list:
+                    new_folder = folder.replace('bulk', 'slab')+'_shift%s' \
+                                                                %(slab.shift)
+                    mplb.write_input(slab, new_folder)
+                    fw = Firework([RunCustodianTask(dir=new_folder,
+                                                    **custodian_params),
+                                   VaspSlabDBInsertTask(struct_type="slab_cell",
+                                                        loc=new_folder, shift=slab.shift,
+                                                        surface_area=slab.surface_area,
+                                                        vsize=slabs.min_vac_size,
+                                                        ssize=slabs.min_slab_size,
+                                                        **vaspdbinsert_parameters)])
+                    FWs.append(fw)
 
-                new_folder = folder.replace('bulk', 'slab')+'_shift%s' \
-                                                            %(slab.shift)
-                mplb.write_input(slab, new_folder)
-                fw = Firework([RunCustodianTask(dir=new_folder,
-                                                **custodian_params),
-                               VaspSlabDBInsertTask(struct_type="slab_cell",
-                                                    loc=new_folder, shift=slab.shift,
-                                                    surface_area=slab.surface_area,
-                                                    vsize=slabs.min_vac_size,
-                                                    ssize=slabs.min_slab_size,
-                                                    **vaspdbinsert_parameters)])
-                FWs.append(fw)
-
-            return FWAction(additions=FWs)
+                return FWAction(additions=FWs)
 
 
 @explicit_serialize
