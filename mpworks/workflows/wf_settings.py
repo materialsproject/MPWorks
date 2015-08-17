6815480eae0c9e322adbd3e2f46c6049cb5cee43
Patrick Huck <phuck@lbl.gov>
2015-08-17 13:53:47 -0700

    wf_settings: adapt QA configs

--- mpworks/workflows/wf_settings.py
+++ mpworks/workflows/wf_settings.py
@@ -5,10 +5,10 @@ __maintainer__ = 'Anubhav Jain'
 __email__ = 'ajain@lbl.gov'
 __date__ = 'May 10, 2013'
 
-QA_VASP = {'nnodes': 2}  # don't change nnodes unless other parts of code are also changed
-QA_VASP_SMALL = {'nnodes': 2, 'walltime': '72:00:00'}  # small walltime jobs
-QA_DB = {'nnodes': 1, 'walltime': '2:00:00',
-         'pre_rocket': '#PBS -V\nulimit -v hard\nmodule load python/2.7.3\nsource $VENV_LOC'}
+QA_VASP = {'nnodes': 1, 'walltime': '48:00:00'}  # don't change nnodes unless other parts of code are also changed
+QA_VASP_SMALL = QA_VASP #{'nnodes': 2, 'walltime': '72:00:00'}  # small walltime jobs
+QA_DB = {'nnodes': 1, 'walltime': '2:00:00'}
+         #'pre_rocket': '#PBS -V\nulimit -v hard\nmodule load python/2.7.3\nsource $VENV_LOC'}
 QA_CONTROL = {'nnodes': 1, 'walltime': '00:30:00'}
 MOVE_TO_GARDEN_DEV = True
 MOVE_TO_GARDEN_PROD = False
