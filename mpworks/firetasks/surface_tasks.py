ce8f9e4a53e7c239a62b8eecc4fe970a0aefb3b0
Richard Tran <rit634989@gmail.com>
2015-09-17 16:58:27 -0700

    will create slab from CONTCAR.relax2 or CONTCAR.relax2.gz

--- mpworks/firetasks/surface_tasks.py
+++ mpworks/firetasks/surface_tasks.py
@@ -285,7 +285,10 @@ class WriteSlabVaspInputs(FireTaskBase):
         # into SlabGenerator is the same as obtaining a slab in the
         # orienetation of the original miller index.
         print 'about to copy contcar'
-        contcar = Poscar.from_file("%s/CONTCAR.relax2.gz" %(cwd+folder))
+        if os.path.exists("%s/CONTCAR.relax2.gz" %(cwd+folder)):
+            contcar = Poscar.from_file("%s/CONTCAR.relax2.gz" %(cwd+folder))
+        else:
+            contcar = Poscar.from_file("%s/CONTCAR.relax2" %(cwd+folder))
         relax_orient_uc = contcar.structure
         print 'made relaxed oriented structure'
         print relax_orient_uc
