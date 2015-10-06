d4a1c7e650a6dc4f805b84237677f073029a374e
Richard Tran <rit634989@gmail.com>
2015-10-06 02:12:10 -0700

    more prints

--- mpworks/firetasks/surface_tasks.py
+++ mpworks/firetasks/surface_tasks.py
@@ -236,7 +236,7 @@ class WriteSlabVaspInputs(FireTaskBase):
         # cell is already oriented with the miller index, entering (0,0,1)
         # into SlabGenerator is the same as obtaining a slab in the
         # orienetation of the original miller index.
-        print 'about to copy contcar'
+        print 'about to copy contcar from %s' %(cwd+folder)
         if os.path.exists("%s/CONTCAR.relax2.gz" %(cwd+folder)):
             contcar = Poscar.from_file("%s/CONTCAR.relax2.gz" %(cwd+folder))
         else:
