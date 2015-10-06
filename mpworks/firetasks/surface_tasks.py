da4544dae94c3093241dee5fd134f8171d6788be
Richard Tran <rit634989@gmail.com>
2015-10-06 11:41:49 -0700

    changes tolerance again

--- mpworks/firetasks/surface_tasks.py
+++ mpworks/firetasks/surface_tasks.py
@@ -257,7 +257,7 @@ class WriteSlabVaspInputs(FireTaskBase):
         # Whether or not to create a list of Fireworks
         # based on different slab terminations
         print 'deciding terminations'
-        slab_list = slabs.get_slabs(tol=0.2) if terminations else [slabs.get_slab(tol=0.2)]
+        slab_list = slabs.get_slabs(tol=0.25) if terminations else [slabs.get_slab(tol=0.25)]
 
         qe = QueryEngine(**vaspdbinsert_parameters)
         optional_data = ["state"]
