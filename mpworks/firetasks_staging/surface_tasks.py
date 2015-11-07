30713c6044bb037549c98a8b775e4ee7652a35d5
Richard Tran <rit634989@gmail.com>
2015-11-06 22:18:44 -0800

    change tolerance for primitive slabs

--- mpworks/firetasks_staging/surface_tasks.py
+++ mpworks/firetasks_staging/surface_tasks.py
@@ -303,7 +303,7 @@ class WriteSlabVaspInputs(FireTaskBase):
         # Whether or not to create a list of Fireworks
         # based on different slab terminations
         print 'deciding terminations'
-        slab_list = slabs.get_slabs() if terminations else [slabs.get_slab(tol=0.25)]
+        slab_list = slabs.get_slabs() if terminations else [slabs.get_slab(tol=0.5)]
 
         print 'chemical formula', relax_orient_uc.composition.reduced_formula
         print 'mpid', mpid
