8e4d4dffe790bc111e8a157dede474b7c455008a
Patrick Huck <phuck@lbl.gov>
2015-11-20 11:08:57 -0800

    save progress on fix_bs_controller_tasks.py

--- mpworks/fix_scripts/fix_bs_controller_tasks.py
+++ mpworks/fix_scripts/fix_bs_controller_tasks.py
@@ -1,3 +1,4 @@
+import time
 from fireworks.core.launchpad import LaunchPad
 from fireworks.core.firework import Firework, Workflow
 from mpworks.firetasks.controller_tasks import AddEStructureTask
@@ -5,29 +6,58 @@ from fireworks.utilities.fw_utilities import get_slug
 
 # DONE manually: "mp-987" -> fw_id: 119629
 
+lpdb = LaunchPad.from_file('/global/homes/m/matcomp/mp_prod/config/config_Mendel/my_launchpad.yaml')
+spec = {'task_type': 'Controller: add Electronic Structure v2', '_priority': 100000}
+
+def append_wf(fw_id, parent_fw_id=None):
+    wf = lpdb.workflows.find_one({'nodes':fw_id}, {'parent_links':1,'links':1,'name':1})
+    try:
+        if parent_fw_id is None:
+            parent_fw_id = wf['parent_links'][str(fw_id)][-1]
+        # non-defused AddEStructureTask v2 already in children?
+        for child_fw_id in wf['links'][str(parent_fw_id)]:
+            if child_fw_id == parent_fw_id: continue
+            child_fw = lpdb.fireworks.find_one({'fw_id': child_fw_id}, {'spec.task_type':1, 'state':1})
+            if child_fw['spec']['task_type'] == 'Controller: add Electronic Structure v2':
+                if child_fw['state'] == 'DEFUSED':
+                    #lpdb.reignite_fw(child_fw_id)
+                    print 'AddEStructureTask v2', child_fw_id , 'reignited for', fw_id
+                else:
+                    print 'AddEStructureTask v2 already added for', fw_id
+                return
+        f = lpdb.get_wf_summary_dict(fw_id)['name'].replace(' ', '_')
+        name = get_slug(f + '--' + spec['task_type'])
+        fw = Firework([AddEStructureTask()], spec, name=name)
+        print fw_id, name, parent_fw_id
+        #lpdb.append_wf(Workflow([fw]), [parent_fw_id])
+    except ValueError:
+        raise ValueError('could not append controller task to wf', wf['name'])
+
 if __name__ == "__main__":
-    lpdb = LaunchPad.from_file('/global/homes/m/matcomp/mp_prod/config/config_Mendel/my_launchpad.yaml')
-    spec = {'task_type': 'Controller: add Electronic Structure v2', '_priority': 100000}
     nfws = 0
+    #append_wf(42391, parent_fw_id=69272)
+    #append_wf(51449, parent_fw_id=76078)
+    #for doc in lpdb.fireworks.find(
+    #    {'spec.task_type': 'Controller: add Electronic Structure v2', 'spec._priority': {'$exists':1}},
+    #    {'fw_id': 1, 'spec._priority': 1, 'state': 1 }
+    #):
+    #    if (doc['state'] == 'FIZZLED' or doc['state'] == 'READY') and doc['spec']['_priority'] == 100000:
+    #        print nfws, doc['fw_id']
+    #        lpdb.defuse_fw(doc['fw_id'])
+    #        nfws += 1
     for doc in lpdb.fireworks.find(
         {'spec.task_type': 'Controller: add Electronic Structure', 'state': 'COMPLETED', 'spec.analysis': {'$exists':1}},
-	{'fw_id': 1, 'spec.analysis.bandgap': 1}
+        {'fw_id': 1, 'spec.analysis.bandgap': 1}
     ):
         fw_id, bandgap = doc['fw_id'], doc['spec']['analysis']['bandgap'] 
         if bandgap > 0 and bandgap < 0.5:
-            old_wf = lpdb.workflows.find_one({'nodes':fw_id}, {'parent_links':1,'name':1})
-            parent_fw_id = old_wf['parent_links'][str(fw_id)][0]
-            f = lpdb.get_wf_summary_dict(fw_id)['name'].replace(' ', '_')
-            name = get_slug(f + '--' + spec['task_type'])
-            fw = Firework([AddEStructureTask()], spec, name=name)
-            print fw_id, name, parent_fw_id
             try:
-                lpdb.append_wf(Workflow([fw]), [parent_fw_id])
+                append_wf(fw_id)
+                time.sleep(.5)
             except ValueError:
-                print 'could not append controller task to wf', old_wf['name']
                 continue
-	    nfws += 1
-	    #break
+            nfws += 1
+            if nfws > 5: break
     print 'nfws =', nfws
 
 # TODO set priorities for child FWs after Controller task has completed
