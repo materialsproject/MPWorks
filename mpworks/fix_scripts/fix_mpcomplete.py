7837f4983e49d62d9b35a3e12e58d748c9fd6bb9
Patrick Huck <phuck@lbl.gov>
2016-04-20 10:55:58 -0700

    add fix_mpcomplete script

--- mpworks/fix_scripts/fix_mpcomplete.py
+++ mpworks/fix_scripts/fix_mpcomplete.py
@@ -1,3 +1,5 @@
+print 'fixing fireworks documents for MPComplete runs on XSEDE ...'
+
 import os, json, sys
 from pymongo import DESCENDING, ASCENDING
 from fireworks.fw_config import CONFIG_FILE_DIR, SORT_FWS
@@ -8,7 +10,6 @@ from datetime import datetime
 from dateutil import parser
 from collections import Counter
 
-print CONFIG_FILE_DIR
 launchpad = LaunchPad.from_file(os.path.join(CONFIG_FILE_DIR, 'my_launchpad.yaml'))
 fworker = FWorker.from_file(os.path.join(CONFIG_FILE_DIR, 'my_fworker.yaml'))
 #print launchpad._get_a_fw_to_run(query=fworker.query, checkout=False)
@@ -26,8 +27,9 @@ projection = {
     'updated_on': 1, 'state': 1
 }
 
-for idoc, doc in enumerate(launchpad.fireworks.find(m_query, projection=projection, sort=sortby)):
-    print doc
+fw_ids = []
+for idoc, doc in enumerate(launchpad.fireworks.find(m_query, projection=projection, sort=sortby).limit(100)):
+    #print doc
     if 'walltime' in doc['spec']['_queueadapter']:
         walltime = doc['spec']['_queueadapter']['walltime'] 
         if int(walltime.split(':')[0]) > 48:
@@ -37,7 +39,7 @@ for idoc, doc in enumerate(launchpad.fireworks.find(m_query, projection=projecti
                 projection=projection,
                 return_document=ReturnDocument.AFTER
             )
-            print '    ----> walltime updated'
+            print doc['fw_id'], '----> walltime updated'
     if 'nnodes' in doc['spec']['_queueadapter'] and not 'nodes' in doc['spec']['_queueadapter']:
         launchpad.fireworks.find_one_and_update(
             {'fw_id': doc['fw_id']}, 
@@ -45,7 +47,7 @@ for idoc, doc in enumerate(launchpad.fireworks.find(m_query, projection=projecti
             projection=projection,
             return_document=ReturnDocument.AFTER
         )
-        print '    ----> nodes key renamed' 
+        print doc['fw_id'], '----> nodes key renamed' 
     if 'pre_rocket' in doc['spec']['_queueadapter']:
         launchpad.fireworks.find_one_and_update(
             m_query,
@@ -53,15 +55,15 @@ for idoc, doc in enumerate(launchpad.fireworks.find(m_query, projection=projecti
             projection=projection,
             return_document=ReturnDocument.AFTER
         )
-        print '    ----> pre_rocket dropped' 
+        print doc['fw_id'], '----> pre_rocket dropped' 
     if 'prev_vasp_dir' in doc['spec'] and not os.path.exists(doc['spec']['prev_vasp_dir']):
         block_dir = doc['spec']['prev_vasp_dir'].split('/')[-2:]
         launch_dir = '/'.join('/oasis/projects/nsf/csd436/phuck/garden'.split('/') + block_dir)
         if not os.path.exists(launch_dir):
-            print '    ---->', '/'.join(block_dir), 'does not exists!'
-            break
-    if idoc > 25:
-        break
+            print doc['fw_id'], '---->', '/'.join(block_dir), 'does not exists!'
+	    continue
+    fw_ids.append(doc['fw_id'])
+print 'fixed', fw_ids
 
 sys.exit(0)
 m_query.pop('state')
